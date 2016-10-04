5c23612b97658af8c4b4e507cc395cb6e400768b
emdelaney <emdelaney@users.noreply.github.com>
2016-10-04 10:02:21 -0700

    Add new files for slicer support.

---
+++ EPPlus/Drawing/ExcelSlicerCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace OfficeOpenXml.Drawing
+{
+	public class ExcelSlicerCache: XmlHelper
+	{
+		#region Constructors
+		internal ExcelSlicerCache(XmlNode node, XmlNamespaceManager namespaceManager): base(namespaceManager, node)
+		{
+
+		}
+		#endregion
+	}
+}
