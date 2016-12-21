4ae2c0ac86a2a738268b1b46efae39e2edc02fc9
emdelaney <emdelaney@localhost>
2016-12-21 14:48:42 -0800

    Fix an issue where relative named workbook range references would be saved as absolute references.

--- EPPlusTest/WorkbookTests.cs
+++ EPPlusTest/WorkbookTests.cs
@@ -6,35 +6,53 @@ using OfficeOpenXml;
 
 namespace EPPlusTest
 {
-    [TestClass]
-    public class WorkbookTests
-    {
-        [TestMethod]
-        public void WorkbookReferenceInNamedRangeDoesNotCorruptWorkbook()
-        {
-            var file = new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\Workbooks\ExternalLinks.xlsx"));
-            var newFile = new FileInfo("test.xlsx");
-            if (newFile.Exists)
-                newFile.Delete();
-            Assert.IsTrue(file.Exists);
-            try
-            {
-                using (var package = new ExcelPackage(file))
-                {
-                    Assert.AreEqual("'[F:\\Cgypt\\Desktop\\test%20cases\\Demo%20Waterfall%20Chart.xlsx]Chart'!$A$1", package.Workbook.Names.First().Formula);
-                    package.SaveAs(newFile);
-                }
-                using (var package = new ExcelPackage(newFile))
-                {
-                    Assert.AreEqual("'[F:\\Cgypt\\Desktop\\test%20cases\\Demo%20Waterfall%20Chart.xlsx]Chart'!$A$1", package.Workbook.Names.First().Formula);
-                }
+	[TestClass]
+	public class WorkbookTests
+	{
+		[TestMethod]
+		public void WorkbookReferenceInNamedRangeDoesNotCorruptWorkbook()
+		{
+			var file = new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\Workbooks\ExternalLinks.xlsx"));
+			var newFile = new FileInfo("test.xlsx");
+			if (newFile.Exists)
+				newFile.Delete();
+			Assert.IsTrue(file.Exists);
+			try
+			{
+				using (var package = new ExcelPackage(file))
+				{
+					Assert.AreEqual("'[F:\\Cgypt\\Desktop\\test%20cases\\Demo%20Waterfall%20Chart.xlsx]Chart'!$A$1", package.Workbook.Names.First().Formula);
+					package.SaveAs(newFile);
+				}
+				using (var package = new ExcelPackage(newFile))
+				{
+					Assert.AreEqual("'[F:\\Cgypt\\Desktop\\test%20cases\\Demo%20Waterfall%20Chart.xlsx]Chart'!$A$1", package.Workbook.Names.First().Formula);
+				}
 
-            }
-            finally
-            {
-                if (newFile.Exists)
-                    newFile.Delete();
-            }
-        }
-    }
+			}
+			finally
+			{
+				if (newFile.Exists)
+					newFile.Delete();
+			}
+		}
+
+		[TestMethod]
+		public void SavePreservesRelativeWorkbookLevelNamedRanges()
+		{
+			var file = new FileInfo(Path.GetTempFileName());
+			file.Delete();
+			using (var package = new ExcelPackage())
+			{
+				var sheet = package.Workbook.Worksheets.Add("Sheet");
+				package.Workbook.Names.Add("MyNamedRange", new ExcelRangeBase(sheet, "$C1"));
+				Assert.AreEqual("'Sheet'!$C1", package.Workbook.Names["MyNamedRange"].FullAddress);
+				package.SaveAs(file);
+			}
+			using (var package = new ExcelPackage(file))
+			{
+				Assert.AreEqual("'Sheet'!$C1", package.Workbook.Names["MyNamedRange"].FullAddress);
+			}
+		}
+	}
 }
