4e891bafbaef0f2bd8406824f0fcb85c35d827f1
colewfrench <colewfrench@users.noreply.github.com>
2017-05-19 10:28:44 -0700

    Make changes to DAY function to pass all tests.

--- EPPlus/FormulaParsing/Excel/Functions/DateTime/Day.cs
+++ EPPlus/FormulaParsing/Excel/Functions/DateTime/Day.cs
@@ -22,20 +22,47 @@
  *******************************************************************************
  * Mats Alm   		                Added		                2013-12-03
  *******************************************************************************/
+using System;
 using System.Collections.Generic;
 using OfficeOpenXml.FormulaParsing.ExpressionGraph;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	public class Day : DateParsingFunction
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="arguments"></param>
+		/// <param name="context"></param>
+		/// <returns></returns>
 		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
 		{
-			if (ValidateArguments(arguments, 1) == false)
-				return new CompileResult(eErrorType.Value);
-			var dateObj = GetFirstValue(arguments);
-			var date = ParseDate(arguments, dateObj);
-
+			if (this.ValidateArguments(arguments, 1) == false)
+				return new CompileResult(eErrorType.Value);			
+			var serialNumberCandidate = this.GetFirstValue(arguments);
+			if (serialNumberCandidate is string)
+			{
+				var isValidDate = System.DateTime.TryParse(serialNumberCandidate.ToString(), out System.DateTime strDate);
+				var isValidNum = Double.TryParse(serialNumberCandidate.ToString(), out double dateAsNum);
+				if (isValidNum)
+					serialNumberCandidate = dateAsNum;
+				else if (!isValidDate)
+					return new CompileResult(eErrorType.Value);
+			}
+			if (serialNumberCandidate is int)
+				serialNumberCandidate = (int)serialNumberCandidate * 1.0;
+			if (serialNumberCandidate is double)
+			{
+				if ((double)serialNumberCandidate == 0)
+					return CreateResult(0, DataType.Integer);
+				else if ((double)serialNumberCandidate < 0)
+					return new CompileResult(eErrorType.Num);
+			}
+			var date = this.ParseDate(arguments, serialNumberCandidate);
 			return CreateResult(date.Day, DataType.Integer);
 		}
 	}
--- EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/DayTests.cs
+++ EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/DayTests.cs
@@ -43,7 +43,7 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.DateTimeFunctions
 	{
 		#region Day Function (Execute) Tests
 		[TestMethod]
-		public void DayShouldReturnDayInMonth()
+		public void DayWithDoubleInputReturnsDayInMonth()
 		{
 			var date = new DateTime(2012, 3, 12);
 			var func = new Day();
@@ -53,12 +53,21 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.DateTimeFunctions
 		}
 
 		[TestMethod]
-		public void DayShouldReturnMonthOfYearWithStringParam()
+		public void DayWithDateAsStringReturnsDayOfMonth()
 		{
-			var date = new DateTime(2012, 3, 12);
 			var func = new Day();
-			var result = func.Execute(FunctionsHelper.CreateArgs("2012-03-12"), this.ParsingContext);
+			var args = FunctionsHelper.CreateArgs("2012-03-12");
+			var result = func.Execute(args, this.ParsingContext);
 			Assert.AreEqual(12, result.Result);
+		}
+
+		[TestMethod]
+		public void DayWithDateAsLongStringReturnsDayOfMonth()
+		{
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs("May 19, 2017");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(19, result.Result);
 		}
 
 		[TestMethod]
@@ -73,6 +82,8 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.DateTimeFunctions
 		[TestMethod]
 		public void DayFunctionWorksInDifferentCultureDateFormats()
 		{
+			// Test the case where the date is represented under
+			// different cultures.
 			var currentCulture = CultureInfo.CurrentCulture;
 			try
 			{
@@ -97,11 +108,128 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.DateTimeFunctions
 					ws.Calculate();
 					Assert.AreEqual(15, ws.Cells[5, 3].Value);
 				}
+
+				var de = CultureInfo.CreateSpecificCulture("de-DE");
+				Thread.CurrentThread.CurrentCulture = de;
+				using (var package = new ExcelPackage())
+				{
+					var ws = package.Workbook.Worksheets.Add("Sheet1");
+					ws.Cells[2, 2].Value = "15.1.2014";
+					ws.Cells[5, 3].Formula = "DAY(B2)";
+					ws.Calculate();
+					Assert.AreEqual(15, ws.Cells[5, 3].Value);
+				}
 			}
 			finally
 			{
 				Thread.CurrentThread.CurrentCulture = currentCulture;
 			}
+		}
+
+		[TestMethod]
+		public void DayWithDateAsIntegerReturnsDayOfMonth()
+		{
+			// Test the case where the date is entered as an integer.
+			// Note that 42874 is the OADate for May 19, 2017.
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs(42874);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(19, result.Result);
+		}
+
+		[TestMethod]
+		public void DayWithZeroAsInputReturnsZeroAsDateOfMonth()
+		{
+			// Test the case where zero is the input.
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs(0);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0, result.Result);
+		}
+
+		[TestMethod]
+		public void DayWithNegativeIntegerAsInputReturnsPoundNum()
+		{
+			// Test the case where a negative integer is the input.
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs(-1);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void DayWithNegativeDoubleAsInputReturnsPoundNum()
+		{
+			// Test the case where a negative double is the input
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs(-1.5);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void DayWithNonDateStringAsInputReturnsPoundValue()
+		{
+			// Test the case where a non-date string is the input.
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs("word");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void DayWithEmptyStringAsInputReturnsPoundValue()
+		{
+			// Test the case where an empty string is the input.
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs("");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void DayWithIntegerInStringAsInputReturnsDayOfMonth()
+		{
+			// Test the case where the input is an integer expressed as a string.
+			// Note that 42874 is the OADate representation of May 19, 2017.
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs("42874");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(19, result.Result);
+		}
+
+		[TestMethod]
+		public void DayWithDoubleInStringAsInputReturnsDayOfMonth()
+		{
+			// Test the case where the input is a double expressed as a string.
+			// Note that 42874.34114 is the OADate representation of some time
+			// on May 19, 2017.
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs("42874.43114");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(19, result.Result);
+		}
+
+		[TestMethod]
+		public void DayWithNegativeIntegerInStringAsInputReturnsPoundNum()
+		{
+			// Test the case where the input is a negative integer expressed
+			// as a string.
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs("-1");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void DayWithNegativeDoubleInStringAsInputReturnsPoundNum()
+		{
+			// Test the case where the input is a negative double expressed
+			// as a string.
+			var func = new Day();
+			var args = FunctionsHelper.CreateArgs("-1.5");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
 		}
 		#endregion
 	}
