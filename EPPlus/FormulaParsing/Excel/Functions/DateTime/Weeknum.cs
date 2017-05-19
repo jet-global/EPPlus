e3c0ad5bbe7872694d82e38f18b63bd740a3cbfc
Nicole Kister <kister19@up.edu>
2017-05-19 12:40:54 -0700

    Bugs/711 improve support for weeknum function (#13)
    * Add unit tests for WEEKNUM Function
    
    * All unit tests for WEEKNUM Function are written.
    
    * Fixing functionality for the WEEKDAY Function
    
    * Added support for integer inputs as well as almost all return type values.
    
    * Code passes all unit tests.
    
    * Brought Weeknum and WeeknumTests up to coding standard.
    
    * Weeknum and WeeknumTest are now up to coding standard.

--- EPPlus/FormulaParsing/Excel/Functions/DateTime/Weeknum.cs
+++ EPPlus/FormulaParsing/Excel/Functions/DateTime/Weeknum.cs
@@ -6,17 +6,62 @@ using OfficeOpenXml.FormulaParsing.ExpressionGraph;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 {
+	/// <summary>
+	/// This class contains the formula for computing the weeknumber based on a specified date. 
+	/// </summary>
 	public class Weeknum : ExcelFunction
 	{
+		/// <summary>
+		/// Execute returns the week number based on the user's input.
+		/// </summary>
+		/// <param name="arguments">The user specified date and optional return type.</param>
+		/// <param name="context">Not used, but needed for overriding the method.</param>
+		/// <returns>The week number out of 52 based on the date given.</returns>
 		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
 		{
 			if (ValidateArguments(arguments, 1) == false)
 				return new CompileResult(eErrorType.Value);
+
+			var serialNumberCandidate = arguments.ElementAt(0).Value;
+
+			if (serialNumberCandidate is null)
+				return new CompileResult(eErrorType.NA);
+			else if (serialNumberCandidate is string)
+			{
+				var isDateString = System.DateTime.TryParse(serialNumberCandidate.ToString(), out System.DateTime date1);
+
+				if (!isDateString)
+					return new CompileResult(eErrorType.Value);
+			}
+			
+
 			var dateSerial = ArgToDecimal(arguments, 0);
+
+			if(dateSerial < 0)
+				return new CompileResult(eErrorType.Num);
+
+
 			var date = System.DateTime.FromOADate(dateSerial);
+
+			var calendarType = CalendarWeekRule.FirstDay;
+
 			var startDay = DayOfWeek.Sunday;
 			if (arguments.Count() > 1)
 			{
+
+				var returnType = arguments.ElementAt(1).Value;
+
+				if (returnType is null)
+					return new CompileResult(eErrorType.Num);
+				else if(returnType is string)
+				{
+					var isValidReturnType = Int32.TryParse(returnType.ToString(), out int result);
+					if (!isValidReturnType)
+						return new CompileResult(eErrorType.Value);
+				}
+
+				
+
 				var argStartDay = ArgToInt(arguments, 1);
 				switch (argStartDay)
 				{
@@ -24,6 +69,8 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 						startDay = DayOfWeek.Sunday;
 						break;
 					case 2:
+						startDay = DayOfWeek.Monday;
+						break;
 					case 11:
 						startDay = DayOfWeek.Monday;
 						break;
@@ -42,21 +89,30 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 					case 16:
 						startDay = DayOfWeek.Saturday;
 						break;
+					case 17:
+						startDay = DayOfWeek.Sunday;
+						break;
+					case 21:
+						startDay = DayOfWeek.Thursday;
+						calendarType = CalendarWeekRule.FirstFullWeek;
+						break;
 					default:
-						// Not supported 
+						// Not supported return type
 						return new CompileResult(eErrorType.Num);
 				}
 			}
+
 			if (DateTimeFormatInfo.CurrentInfo == null)
 			{
 				throw new InvalidOperationException(
 					 "Could not execute Weeknum function because DateTimeFormatInfo.CurrentInfo was null");
 			}
-			var week = DateTimeFormatInfo.CurrentInfo.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstDay,
-																								  startDay);
-			return CreateResult(week, DataType.Integer);
+
+			var week = DateTimeFormatInfo.CurrentInfo.Calendar.GetWeekOfYear(date, calendarType, startDay);
+			if (serialNumberCandidate is int)
+				if (dateSerial == 0)
+					week = 0;
+			return this.CreateResult(week, DataType.Integer);
 		}
-
-
 	}
 }
--- EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/WeeknumTests.cs
+++ EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/WeeknumTests.cs
@@ -25,6 +25,8 @@
 * For code change notes, see the source control history.
 *******************************************************************************/
 using System;
+using System.Globalization;
+using System.Threading;
 using EPPlusTest.Excel.Functions.DateTimeFunctions;
 using EPPlusTest.FormulaParsing.TestHelpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -37,22 +39,313 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.DateTimeFunctions
 	public class WeeknumTests : DateTimeFunctionsTestBase
 	{
 		#region Weeknum Function (Execute) Tests
-		[TestMethod]
-		public void WeekNumShouldReturnCorrectResult()
-		{
-			var func = new Weeknum();
-			var dt1 = new DateTime(2012, 12, 31).ToOADate();
-			var dt2 = new DateTime(2012, 1, 1).ToOADate();
-			var dt3 = new DateTime(2013, 1, 20).ToOADate();
 
-			var r1 = func.Execute(FunctionsHelper.CreateArgs(dt1), this.ParsingContext);
-			var r2 = func.Execute(FunctionsHelper.CreateArgs(dt2), this.ParsingContext);
-			var r3 = func.Execute(FunctionsHelper.CreateArgs(dt3, 2), this.ParsingContext);
-			var r4 = func.Execute(FunctionsHelper.CreateArgs(), this.ParsingContext);
-			Assert.AreEqual(53, r1.Result, "r1.Result was not 53, but " + r1.Result.ToString());
-			Assert.AreEqual(1, r2.Result, "r2.Result was not 1, but " + r2.Result.ToString());
-			Assert.AreEqual(3, r3.Result, "r3.Result was not 3, but " + r3.Result.ToString());
-			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)r4.Result).Type);
+		//The below tests do not include a second parameter (return type)
+
+		[TestMethod]
+		public void WeekNumWtihNoInputReturnsPoundValue()
+		{
+			//Test case where there is no input into the weeknum function.
+			var function = new Weeknum();
+			var result = function.Execute(FunctionsHelper.CreateArgs(), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void WeekNumWithDateFunctionInputReturnsCorrectResult()
+		{
+			//Test case where the input is a DateTime from the DateTime function.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(1, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithDateAsStringReturnsCorrectResult()
+		{
+			//Test case where the input is a date written as a string.
+			var function = new Weeknum();
+			var date = "1/10/2017";
+			var result = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithDateAsStringWithDashesReturnsCorrectResult()
+		{
+			// Test case where the input is a date is written with '-' instead of '/'.		
+			var function = new Weeknum();
+			var date = "1-10-2017";
+			var result = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithStringArgumentReturnsPoundValue()
+		{
+			// Test case where the input is a general string and empty string.
+			var function = new Weeknum();
+			var date1 = "testString";
+			var date2 = "";
+			var result = function.Execute(FunctionsHelper.CreateArgs(date1), this.ParsingContext);
+			var r2 = function.Execute(FunctionsHelper.CreateArgs(date2), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)r2.Result).Type);
+		}
+
+		[TestMethod]
+		public void WeekNumWithOADateArgumentReturnsCorrectValue()
+		{
+			//Test case with the input as an OADate.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 10).ToOADate();
+			var result = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithNonZeroIntegerArgumentReturnsCorrectValue()
+		{
+			//Test case where the input is a non-zero integer.
+			var function = new Weeknum();
+			var date = 365;
+			var result = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(53, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithZeroIntegerReturnsZero()
+		{
+			//Test case where the input is zero.
+			var function = new Weeknum();
+			var date = 0;
+			var result = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(0, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithNegativeIntReturnsPoundNum()
+		{
+			// Test case where the input is a negative integer.
+			var function = new Weeknum();
+			var date = -5;
+			var result = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void WeekNumWithDateNotAsStringReturnsCorrectValue()
+		{
+			// Test case where the date is written not in string form.
+			var function = new Weeknum();
+			var date = 1 / 5 / 2017;
+			var result = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(0, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithPeriodInsteadOfDashesOnUSCultureCorrecctValue()
+		{
+			// Test case where the input is the date written with '.' instead of '/'.
+			//This functionality differs from Excel's. Excel normally returns a #VALUE! on the US 
+			//Culture, however the Weeknum class in EPPlus returns the week number. 
+			var function = new Weeknum();
+			var date = "1.5.2017";
+			var result = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(1, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithItalianhDateAsStringWithPeriodsReturnsCorrectResult()
+		{
+			// Test case where the culture has been changed to Italian to test the 
+			// Italian date format.
+			var currentCulture = Thread.CurrentThread.CurrentCulture;
+			try
+			{
+				Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("it-IT");
+				var function = new Weekday();
+				var args = FunctionsHelper.CreateArgs("3.5.2017");
+				var result = function.Execute(args, this.ParsingContext);
+				Assert.AreEqual(4, result.Result);
+			}
+			finally
+			{
+				Thread.CurrentThread.CurrentCulture = currentCulture;
+			}
+		}
+
+		//Below are the tests that include the second parameter (return type)
+
+		[TestMethod]
+		public void WeekNumWithReturnType1OrOmmittedReturnsCorrectValue()
+		{
+			// Test case with the a return type of 1 or an ommitted return type.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 1), this.ParsingContext);
+			var r2 = function.Execute(FunctionsHelper.CreateArgs(date), this.ParsingContext);
+			Assert.AreEqual(1, result.Result);
+			Assert.AreEqual(1, r2.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithReturnType2ReturnsCorrectValue()
+		{
+			// Test case where the return type is 2.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 2), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithReturnType11ReturnsCorrectValue()
+		{
+			// Test case where the return type is 11.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 11), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithReturnType12ReturnsCorrectValue()
+		{
+			// Test case where the return type is 12.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 12), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithReturnType13ReturnsCorrectValue()
+		{
+			//Test case where the reutrn type is 13.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 13), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithReturnType14ReturnsCorrectValue()
+		{
+			// Test case where the return type is 14.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 14), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithReturnType15ReturnsCorrectValue()
+		{
+			// Test case where the return type is 15.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 15), this.ParsingContext);
+			Assert.AreEqual(1, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithReturnType16ReturnsCorrectValue()
+		{
+			// Test case where the return type is 16.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 16), this.ParsingContext);
+			Assert.AreEqual(1, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithReturnType17ReturnsCorrectValue()
+		{
+			// Test case where the return type is 17.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 17), this.ParsingContext);
+			Assert.AreEqual(1, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithReturnType21ReturnsCorrectValue()
+		{
+			// Test case where the return type is 21.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 21), this.ParsingContext);
+			Assert.AreEqual(1, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithInvalidReturnTypeReturnsPoundNum()
+		{
+			// Test case with an invalid return type.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, 5), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void WeekNumWithNumericStringReturnTypeReturnsCorrectValue()
+		{
+			// Test case where the return type is in string format.
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, "1"), this.ParsingContext);
+			Assert.AreEqual(1, result.Result);
+		}
+
+		[TestMethod]
+		public void WeekNumWithStringReturnTypeReturnsPoundValue()
+		{
+			// Test case where the return type is a general string
+			var function = new Weeknum();
+			var date = new DateTime(2017, 1, 5);
+			var result = function.Execute(FunctionsHelper.CreateArgs(date, "testString"), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void WeekNumWithNoFirstParameterAndValidReturnTypeReturnsPoundNA()
+		{
+			// Test case where the first param is null with a valid return type.
+			var function = new Weeknum();
+			var result = function.Execute(FunctionsHelper.CreateArgs(null, 1), this.ParsingContext);
+			Assert.AreEqual(eErrorType.NA, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void WeekNumWithNoFirstParameterAndInvalidReturnTypeReturnsPoundNA()
+		{
+			// Test case where the first param is null with an invalid return type. 
+			var function = new Weeknum();
+			var result = function.Execute(FunctionsHelper.CreateArgs(null, 5), this.ParsingContext);
+			Assert.AreEqual(eErrorType.NA, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void WeekNumWithNegativeIntAndValidReturnTypeReutrnsPoundNum()
+		{
+			// Test case where the first param is a negative integer and has a valid
+			// return type.
+			var function = new Weeknum();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-1, 1), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void WeekNumWithNegativIntAndInvalidReturnTypeReturnsPoundNum()
+		{
+			// Test case where the first param is a negative integer and has an invalid
+			// return type. 
+			var function = new Weeknum();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-1, 5), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
 		}
 		#endregion
 	}
