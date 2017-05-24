77bce56c134e5e6b2d364508ce1311c12411b50e
Nicole Kister <NaKister@users.noreply.github.com>
2017-05-24 08:49:46 -0700

    The Workday class is now up to coding standard.

--- EPPlus/FormulaParsing/Excel/Functions/DateTime/Workday.cs
+++ EPPlus/FormulaParsing/Excel/Functions/DateTime/Workday.cs
@@ -6,57 +6,62 @@ using OfficeOpenXml.Utils;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 {
+	/// <summary>
+	/// This class contains the formula for computing a date based on the given date, number of workdays, and (optional).
+	/// dates of holidays
+	/// </summary>
 	public class Workday : ExcelFunction
 	{
+		/// <summary>
+		/// Execute returns the date based on the user's input.
+		/// 
+		/// </summary>
+		/// <param name="arguments">The user specified date, number of workdays, and optional dates of holidays</param>
+		/// <param name="context">Not used, but needed for overriding the method.</param>
+		/// <returns>The date based on the date given and the number of workdays and optional holidays.</returns>
 		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
 		{
 			var functionArguments = arguments as FunctionArgument[] ?? arguments.ToArray();
 
 			if (ValidateArguments(arguments, 2) == false)
 				return new CompileResult(eErrorType.Value);
-			if (arguments.ElementAt(1).Value is null)
-				return new CompileResult(eErrorType.NA);
 
 			var serialNumberCandidate = arguments.ElementAt(0).Value;
+			var workDaysCandidate = arguments.ElementAt(1).Value;
+
+			if (workDaysCandidate is null)
+				return new CompileResult(eErrorType.NA);
 			if (serialNumberCandidate == null)
 				return new CompileResult(eErrorType.NA);
+
 			else if (serialNumberCandidate is string)
 			{
-				var isDateString = ConvertUtil.TryParseDateString(serialNumberCandidate.ToString(), out System.DateTime output);
-				if (!isDateString)
+				if(!ConvertUtil.TryParseDateString(serialNumberCandidate.ToString(), out System.DateTime output))
 					return new CompileResult(eErrorType.Value);
 			}
-			else if (serialNumberCandidate is int)
-				if (ArgToInt(functionArguments, 1) < 0)
+			else if (serialNumberCandidate is int && ArgToInt(functionArguments, 1) < 0)
 					return new CompileResult(eErrorType.Num);
+
+			if (workDaysCandidate is string)
+			{
+				if(!ConvertUtil.TryParseDateString(workDaysCandidate.ToString(), out System.DateTime output2))
+					return new CompileResult(eErrorType.Value);
+			}
 
 			var dateSerial = ArgToDecimal(arguments, 0);
 			if (dateSerial < 0)
 				return new CompileResult(eErrorType.Num);
-
-			var workDaysCandidate = arguments.ElementAt(1).Value;
-			if (workDaysCandidate is string)
-			{
-				var isWorkDayString = ConvertUtil.TryParseDateString(workDaysCandidate.ToString(), out System.DateTime output2);
-				if (!isWorkDayString)
-					return new CompileResult(eErrorType.Value);
-			}
-			
-
 			if (dateSerial == 0)
 				if (ArgToInt(arguments, 1) < 0)
 					return new CompileResult(eErrorType.Num);
 
 			var startDate = System.DateTime.FromOADate(dateSerial);
-
 			var workDateSerial = ArgToDecimal(arguments, 1);
-
 			var resultDate = System.DateTime.MinValue;
-
 			var calculator = new WorkdayCalculator();
 			var result = calculator.CalculateWorkday(startDate, (int)workDateSerial);
 
-			bool test = false;
+			bool holidayStringTest = false;
 			if (functionArguments.Length > 2)
 			{
 				for (int i = 2; i < functionArguments.Length; i++)
@@ -67,7 +72,7 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 						var isHolidayString = ConvertUtil.TryParseDateString(holidayCandidate, out System.DateTime output3);
 						if (!isHolidayString)
 							return new CompileResult(eErrorType.Value);
-						test = true;
+						holidayStringTest = true;
 					}
 					else if (holidayCandidate is int)
 					{
@@ -79,10 +84,11 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 				}
 				result = calculator.AdjustResultWithHolidays(result, functionArguments[2]);
 			}
+
 			if (dateSerial == 0)
 				if (dateSerial % 5 == 0)
 					return CreateResult(result.EndDate.ToOADate()-1, DataType.Date);
-			if (test)
+			if (holidayStringTest)
 				return CreateResult(result.EndDate.ToOADate()+1, DataType.Date);
 			return CreateResult(result.EndDate.ToOADate(), DataType.Date);
 		}
