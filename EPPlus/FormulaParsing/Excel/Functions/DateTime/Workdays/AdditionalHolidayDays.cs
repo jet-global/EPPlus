c71a61cf37226e356036916728dbda07d3d31878
Nicole Kister <NaKister@users.noreply.github.com>
2017-05-24 12:28:45 -0700

    AdditionalHolidayDays is now up to coding standard.

--- EPPlus/FormulaParsing/Excel/Functions/DateTime/Workdays/AdditionalHolidayDays.cs
+++ EPPlus/FormulaParsing/Excel/Functions/DateTime/Workdays/AdditionalHolidayDays.cs
@@ -4,11 +4,17 @@ using OfficeOpenXml.Utils;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime.Workdays
 {
+	/// <summary>
+	/// This class takes a FunctionArgument list of dates and turns them into a list of DateTime objects. 
+	/// </summary>
 	public class AdditionalHolidayDays
 	{
 		private readonly FunctionArgument _holidayArg;
 		private readonly List<System.DateTime> _holidayDates = new List<System.DateTime>();
-
+		/// <summary>
+		/// The constructor for this class calls the method Initalize, which does the transformation of the list. 
+		/// </summary>
+		/// <param name="holidayArg">The list of FunctionArguments to be turned into DateTime objects.</param>
 		public AdditionalHolidayDays(FunctionArgument holidayArg)
 		{
 			_holidayArg = holidayArg;
@@ -17,11 +23,15 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime.Workdays
 
 		public IEnumerable<System.DateTime> AdditionalDates => _holidayDates;
 
+		/// <summary>
+		/// Initalize takes the list of FunctionArguments and turns it into a list of DateTime objects. It handles the cases 
+		/// where the arguments are dates as OADates, results of the DATE Function, integers, and strings.
+		/// It works for both arrays of dates or dates as values from an Excel worksheet.
+		/// </summary>
 		private void Initialize()
 		{
 			bool testBool = false;
 			var holidays = _holidayArg.Value as IEnumerable<FunctionArgument>;
-
 
 			if (holidays != null)
 			{
@@ -30,7 +40,6 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime.Workdays
 					_holidayDates.Add(holidayDate);
 					testBool = true;
 				}
-
 
 				if (!testBool)
 				{
@@ -42,7 +51,6 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime.Workdays
 					}
 				}
 			}
-
 
 			var range = _holidayArg.Value as ExcelDataProvider.IRangeInfo;
 			if (range != null)
@@ -68,7 +76,6 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime.Workdays
 			{
 				_holidayDates.Add(System.DateTime.FromOADate(ConvertUtil.GetValueDouble(_holidayArg.Value)));
 			}
-
 		}
 	}
 }
