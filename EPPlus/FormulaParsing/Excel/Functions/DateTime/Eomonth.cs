acea773cf41d539d506c085deb9ffafa018ed8c4
emdelaney <emdelaney@users.noreply.github.com>
2017-05-30 13:29:03 -0700

    Add comments and unit tests for the EOMONTH function.

--- EPPlus/FormulaParsing/Excel/Functions/DateTime/Eomonth.cs
+++ EPPlus/FormulaParsing/Excel/Functions/DateTime/Eomonth.cs
@@ -3,8 +3,18 @@ using OfficeOpenXml.FormulaParsing.ExpressionGraph;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 {
+	/// <summary>
+	/// Represents an excel function that gets the end of the month based on a date and a specified month offset. 
+	/// </summary>
 	public class Eomonth : ExcelFunction
 	{
+		#region ExcelFunction Overrides
+		/// <summary>
+		/// Executes the EOMONTH function.
+		/// </summary>
+		/// <param name="arguments">A date object and a number of months to offset from that date.</param>
+		/// <param name="context">Unused; an object that contains information about the context in which the function is being run.</param>
+		/// <returns>An OADate representing the end of the month the specified number of months before or after the specified date.</returns>
 		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
 		{
 			if (ValidateArguments(arguments, 2) == false)
@@ -14,5 +24,6 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 			var resultDate = new System.DateTime(date.Year, date.Month, 1).AddMonths(monthsToAdd + 1).AddDays(-1);
 			return CreateResult(resultDate.ToOADate(), DataType.Date);
 		}
+		#endregion
 	}
 }
