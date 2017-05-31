4e4da1119b165e9693698c33aa09e480259dcd52
Nicole Kister <nicolek@jetreports.com>
2017-05-31 13:37:53 -0700

    Adding unit tests for the Days360 Function.

--- EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/Days360Tests.cs
+++ EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/Days360Tests.cs
@@ -40,106 +40,135 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.DateTimeFunctions
 		[TestMethod]
 		public void Days360ShouldReturnCorrectResultWithNoMethodSpecified()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt1arg = new DateTime(2013, 1, 1).ToOADate();
 			var dt2arg = new DateTime(2013, 3, 31).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg), this.ParsingContext);
 			Assert.AreEqual(90, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360ShouldReturnCorrectResultWithNoMethodSpecifiedMiddleOfMonthDates()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt1arg = new DateTime(1982, 4, 25).ToOADate();
 			var dt2arg = new DateTime(2016, 6, 12).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg), this.ParsingContext);
 			Assert.AreEqual(12287, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360ShouldReturnCorrectResultWithEuroMethodSpecified()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt1arg = new DateTime(2013, 1, 1).ToOADate();
 			var dt2arg = new DateTime(2013, 3, 31).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg, true), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg, true), this.ParsingContext);
 			Assert.AreEqual(89, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360ShouldHandleFebWithEuroMethodSpecified()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt1arg = new DateTime(2012, 2, 29).ToOADate();
 			var dt2arg = new DateTime(2013, 2, 28).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg, true), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg, true), this.ParsingContext);
 			Assert.AreEqual(359, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360ShouldHandleFebWithUsMethodSpecified()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt1arg = new DateTime(2012, 2, 29).ToOADate();
 			var dt2arg = new DateTime(2013, 2, 28).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg, false), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg, false), this.ParsingContext);
 			Assert.AreEqual(358, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360ShouldHandleFebWithUsMethodSpecifiedEndOfMonth()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt1arg = new DateTime(2013, 2, 28).ToOADate();
 			var dt2arg = new DateTime(2013, 3, 31).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg, false), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(dt1arg, dt2arg, false), this.ParsingContext);
 			Assert.AreEqual(30, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360ShouldHandleNullFirstDateArgument()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt2arg = new DateTime(2013, 3, 15).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(null, dt2arg, false), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(null, dt2arg, false), this.ParsingContext);
 			Assert.AreEqual(40755, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360ShouldHandleNullFirstDateArgumentEndOfMonth()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt2arg = new DateTime(2013, 3, 31).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(null, dt2arg, false), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(null, dt2arg, false), this.ParsingContext);
 			Assert.AreEqual(40771, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360ShouldHandleNullSecondDateArgument()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt1arg = new DateTime(1992, 2, 10).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(dt1arg, null, false), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(dt1arg, null, false), this.ParsingContext);
 			Assert.AreEqual(-33160, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360ShouldHandleNullSecondDateArgumentEndOfMonth()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var dt1arg = new DateTime(2013, 2, 28).ToOADate();
-			var result = func.Execute(FunctionsHelper.CreateArgs(dt1arg, null, false), this.ParsingContext);
+			var result = function.Execute(FunctionsHelper.CreateArgs(dt1arg, null, false), this.ParsingContext);
 			Assert.AreEqual(-40740, result.Result);
 		}
 
 		[TestMethod]
 		public void Days360WithInvalidArgumentReturnsPoundValue()
 		{
-			var func = new Days360();
+			var function = new Days360();
 			var args = FunctionsHelper.CreateArgs();
-			var result = func.Execute(args, this.ParsingContext);
+			var result = function.Execute(args, this.ParsingContext);
 			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void Days306WithInputAsResultOfDateFunctionReturnsCorrectValue()
+		{
+			var function = new Days360();
+			var dateArg1 = new DateTime(2017, 5, 31);
+			var dateArg2 = new DateTime(2017, 6, 30);
+			var args = FunctionsHelper.CreateArgs(dateArg1, dateArg2);
+			var result = function.Execute(args, this.ParsingContext);
+			Assert.AreEqual(30, result.Result);
+		}
+
+		[TestMethod]
+		public void Days360WithInputAsDateStringsReturnsCorrectValue()
+		{
+			var function = new Days360();
+			var args = FunctionsHelper.CreateArgs("5/31/2017", "6/30/2017");
+			var result = function.Execute(args, this.ParsingContext);
+			Assert.AreEqual(30, result.Result);
+		}
+
+		[TestMethod]
+		public void Days360WithIntegerInputReturnsCorrectValue()
+		{
+			var function = new Days360();
+			var args = FunctionsHelper.CreateArgs(15, 20);
+			var result = function.Execute(args, this.ParsingContext);
+			Assert.AreEqual(5, result.Result);
 		}
 		#endregion
 	}
