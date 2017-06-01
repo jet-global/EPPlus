d49879bb1a8cb978f4f5ca8cab08813c74209596
colewfrench <colewfrench@users.noreply.github.com>
2017-06-01 13:25:35 -0700

    Add unit tests for the YEARFRAC function.

--- EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/YearFracTests.cs
+++ EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/YearFracTests.cs
@@ -86,6 +86,479 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.DateTimeFunctions
 			var result = func.Execute(FunctionsHelper.CreateArgs(), this.ParsingContext);
 			Assert.AreEqual(eErrorType.Value, (result.Result as ExcelErrorValue).Type);
 		}
+
+		[TestMethod]
+		public void YearFrac()
+		{
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs();
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsIntegerReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs((int)(date1.ToOADate()), (int)(date2.ToOADate()));
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsDoubleReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate());
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsIntegerInStringReturnsCorrectResult()
+		{
+			// Note that 42736 and 42878 are the Excel OADates for 1/1/2017 and 5/23/2017 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs("42736", "42878");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsDoubleInStringReturnsCorrectResult()
+		{
+			// Note that 42736.5 and 42878.5 are the Excel OADates for noon on 1/1/2017 and 5/23/2017 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs("42736.5", "42878.5");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsStringReturnsCorrectResult()
+		{
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs("1/1/2017", "5/23/2017");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsLongStringReturnsCorrectResult()
+		{
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs("January 1, 2017", "May 23, 2017");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithNonNumericStringAsFirstParameterReturnsPoundValue()
+		{
+			// Note that 42878 is the Excel OADate for 5/23/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs("word", 42878);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithNonNumericStringAsSecondParameterReturnsPoundValue()
+		{
+			// Note that 42736 is the Excel OADate for 1/1/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(42736, "word");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithEmptyStringAsFirstParameterReturnsPoundValue()
+		{
+			// Note that 42878 is the Excel OADate for 5/23/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(string.Empty, 42878);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithEmptyStringAsSecondParameterReturnsPoundValue()
+		{
+			// Note that 42736 is the Excel OADate for 1/1/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(42736, string.Empty);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsNegativeIntegerReturnsPoundNum()
+		{
+			// Note that 42878 is the Excel OADate for 5/23/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(-1, 42878);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsNegativeDoubleReturnsPoundNum()
+		{
+			// Note that 42736 is the Excel OADate for 1/1/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(42736, -1.5);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsNegativeIntegerInStringReturnsPoundNum()
+		{
+			// Note that 42878 is the Excel OADate for 5/23/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs("-1", 42878);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsNegativeDoubleInStringReturnsPoundNum()
+		{
+			// Note that 42736 is the Excel OADate for 1/1/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(42736, "-1.5");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithFirstDateLaterThanSecondDateReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 5, 23);
+			var date2 = new DateTime(2017, 1, 1);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate());
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesMoreThanAYearApartReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2019, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate());
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(2.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithNullFirstParameterReturnsPoundNA()
+		{
+			// Note that 42878 is the Excel OADate for 5/23/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(null, 42878);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.NA, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithNullSecondParameterReturnsPoundNA()
+		{
+			// Note that 42736 is Excel OADate for 1/1/2017.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(42736, null);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.NA, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithNullBasisParameterReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), null);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithBothDatesTheSameReturnsCorrectResult()
+		{
+			var date = new DateTime(2017, 1, 1);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date, date);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.0, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsZeroReturnsCorrectResult()
+		{
+			// Note that 42878 is the Excel OADate for 5/23/2017.
+			// 0 is the Excel OADate for 1/0/2017, which Excel treats as a special date.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(0, 42878);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(117.397222222222, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDateAsOneReturnsCorrectResult()
+		{
+			// Note that 1 and 42878 are the Excel OADates for 1/1/1900 and 5/23/2017 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(1, 42878);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(117.394444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithNegativeFirstParameterAndNonNumericStringSecondParameterReturnsPoundNum()
+		{
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(-1, "word");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithNonNumericStringFirstParameterAndNegativeSecondParameterReturnsPoundValue()
+		{
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs("word", -1);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesAfter1March1900WithBasis1ReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(),1);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.389041095890411, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesAfter1March1900WithBasis2ReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), 2);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesAfter1March1900WithBasis3ReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), 3);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.389041095890411, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesAfter1March1900WithBasis4ReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), 4);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.394444444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesBefore1March1900WithBasis0ReturnsCorrectResult()
+		{
+			// Note that 31 and 59 are the Excel OADates for 1/31/1900 and 2/28/1900 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 59, 0);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.0777777777777778, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesBefore1March1900WithBasis1ReturnsCorrectResult()
+		{
+			// Note that 31 and 59 are the Excel OADates for 1/31/1900 and 2/28/1900 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 59, 1);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.076712328767123, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesBefore1March1900WithBasis2ReturnsCorrectResult()
+		{
+			// Note that 31 and 59 are the Excel OADates for 1/31/1900 and 2/28/1900 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 59, 2);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.0777777777777778, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesBefore1March1900WithBasis3ReturnsCorrectResult()
+		{
+			// Note that 31 and 59 are the Excel OADates for 1/31/1900 and 2/28/1900 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 59, 3);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.076712328767123, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesBefore1March1900WithBasis4ReturnsCorrectResult()
+		{
+			// Note that 31 and 59 are the Excel OADates for 1/31/1900 and 2/28/1900 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 59, 4);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.0777777777777778, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesWith1March1900BetweenWithBasis0ReturnsCorrectResult()
+		{
+			// Note that 31 and 42736 are the Excel OADates for 1/31/1900 and 1/1/2017 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 42736, 0);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(116.919444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesWith1March1900BetweenWithBasis1ReturnsCorrectResult()
+		{
+			// Note that 31 and 42736 are the Excel OADates for 1/31/1900 and 1/1/2017 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 42736, 1);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(116.921274275505, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesWith1March1900BetweenWithBasis2ReturnsCorrectResult()
+		{
+			// Note that 31 and 42736 are the Excel OADates for 1/31/1900 and 1/1/2017 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 42736, 2);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(118.625, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesWith1March1900BetweenWithBasis3ReturnsCorrectResult()
+		{
+			// Note that 31 and 42736 are the Excel OADates for 1/31/1900 and 1/1/2017 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 42736, 3);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(117, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithDatesWith1March1900BetweenWithBasis4ReturnsCorrectResult()
+		{
+			// Note that 31 and 42736 are the Excel OADates for 1/31/1900 and 1/1/2017 respectively.
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(31, 42736, 4);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(116.919444444444, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithBasisAsDoubleReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), 1.5);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.389041095890411, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithBasisIntegerInStringReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), "1");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.389041095890411, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithBasisAsDoubleInStringReturnsCorrectResult()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), "1.5");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(0.389041095890411, result.Result);
+		}
+
+		[TestMethod]
+		public void YearFracWithBasisAsNonEnumeratedNumberReturnsPoundNum()
+		{
+			// Note that the YEARFRAC function in Excel only has 0-4 as valid numbers for the basis parameter.
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), 5);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithNegativeBasisNumberReturnsPoundNum()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), -1);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithBasisAsNonNumericStringReturnsPoundValue()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), "word");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void YearFracWithBasisAsEmptyStringReturnsPoundValue()
+		{
+			var date1 = new DateTime(2017, 1, 1);
+			var date2 = new DateTime(2017, 5, 23);
+			var func = new Yearfrac();
+			var args = FunctionsHelper.CreateArgs(date1.ToOADate(), date2.ToOADate(), string.Empty);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
 		#endregion
 	}
 }
