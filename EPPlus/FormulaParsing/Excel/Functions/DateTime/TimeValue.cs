d34a5d34a679e20ed3bf8d708964bca422770e38
Nicholas Accuardi <nicholasa@jetreports.com>
2017-06-01 07:40:32 -0700

    1st Commit - Trying to figure out the last test case.

--- EPPlus/FormulaParsing/Excel/Functions/DateTime/TimeValue.cs
+++ EPPlus/FormulaParsing/Excel/Functions/DateTime/TimeValue.cs
@@ -20,15 +20,47 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 		internal CompileResult Execute(string dateString)
 		{
 			System.DateTime result;
+			
+			
 			System.DateTime.TryParse(dateString, out result);
-			return result != System.DateTime.MinValue ?
-				 CreateResult(GetTimeValue(result), DataType.Date) :
-				 CreateResult(ExcelErrorValue.Create(eErrorType.Value), DataType.ExcelError);
+		
+				return result != System.DateTime.MinValue ?
+					 CreateResult(GetTimeValue(result), DataType.Date) :
+					 CreateResult(ExcelErrorValue.Create(eErrorType.Value), DataType.ExcelError);
+			
+			/*
+			if (System.DateTime.TryParse(dateString, out result))
+			{
+				return result != System.DateTime.MinValue ?
+					 CreateResult(GetTimeValue(result), DataType.Date) :
+					 CreateResult(ExcelErrorValue.Create(eErrorType.Value), DataType.ExcelError);
+
+			}//ELse this is where we need to do somthing cray
+			else
+			{
+
+
+
+
+
+			}
+			*/
+
+
+
+
+
+			return null;
+			
+			
 		}
 
 		private double GetTimeValue(System.DateTime result)
 		{
-			return (int)result.TimeOfDay.TotalSeconds == 0 ? 0d : result.TimeOfDay.TotalSeconds / (3600 * 24);
+			
+			return (int)result.TimeOfDay.TotalSeconds == 0 ? 0d : result.TimeOfDay.TotalSeconds  / (3600 * 24);//3600 is seconds in an hour. So this is seconds in a day.
 		}
 	}
+
+
 }
--- EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/TimeValueTests.cs
+++ EPPlusTest/FormulaParsing/Excel/Functions/DateTimeFunctions/TimeValueTests.cs
@@ -29,6 +29,8 @@ using EPPlusTest.FormulaParsing.TestHelpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfficeOpenXml;
 using OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime;
+using System;
+
 
 namespace EPPlusTest.FormulaParsing.Excel.Functions.DateTimeFunctions
 {
@@ -45,6 +47,165 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.DateTimeFunctions
 			var result = func.Execute(args, this.ParsingContext);
 			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
 		}
+
+		[TestMethod]
+		public void TimeValueIsGivenAString()
+		{
+			var function = new TimeValue();
+			
+			var input1 = "Noon";
+			var input2 = "midnight";
+			var input3 = "one o'clock";
+
+			var result1 = function.Execute(FunctionsHelper.CreateArgs(input1), this.ParsingContext);
+			var result2 = function.Execute(FunctionsHelper.CreateArgs(input2), this.ParsingContext);
+			var result3 = function.Execute(FunctionsHelper.CreateArgs(input3), this.ParsingContext);
+
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result1.Result).Type);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result2.Result).Type);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result3.Result).Type);
+		}
+
+		[TestMethod]
+		public void TimeValueIsGivenMilitaryTime()
+		{
+			var function = new TimeValue();
+
+			var input1 = "00:00";
+			var input2 = "00:01";
+			var input3 = "-00:01";
+			var input4 = "24:00";
+
+			var result1 = function.Execute(FunctionsHelper.CreateArgs(input1), this.ParsingContext);
+			var result2 = function.Execute(FunctionsHelper.CreateArgs(input2), this.ParsingContext);
+			var result3 = function.Execute(FunctionsHelper.CreateArgs(input3), this.ParsingContext);
+			var result4 = function.Execute(FunctionsHelper.CreateArgs(input4), this.ParsingContext);
+			
+			Assert.AreEqual(0.0,result1.ResultNumeric);
+			Assert.AreEqual( 0.00069444, System.Math.Round(result2.ResultNumeric, 8));
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result3.Result).Type);
+			Assert.AreEqual(0.0, result4.ResultNumeric);
+		}
+
+		[TestMethod]
+		public void TimeValueMillitaryTimePast2400ActsLikeItWasModded()
+		{
+			var function = new TimeValue();
+
+			var input1 = "25:00";
+			var input2 = "01:00";
+			var input3 = "26:00";
+			var input4 = "02:00";
+
+			var result1 = function.Execute(FunctionsHelper.CreateArgs(input1), this.ParsingContext);
+			var result2 = function.Execute(FunctionsHelper.CreateArgs(input2), this.ParsingContext);
+			var result3 = function.Execute(FunctionsHelper.CreateArgs(input3), this.ParsingContext);
+			var result4 = function.Execute(FunctionsHelper.CreateArgs(input4), this.ParsingContext);
+
+			//Assert.AreEqual(result1.ResultNumeric, result2.ResultNumeric);
+			//Assert.AreEqual(result3.ResultNumeric, result4.ResultNumeric);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result1.Result).Type);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result3.Result).Type);
+
+		}
+
+		[TestMethod]
+		public void TimeValueDatesAreIgnored()
+		{
+			var function = new TimeValue();
+
+			var input1 = "1/11/2011 2:00";
+			var input2 = "1/11/2011 2:00 am";
+			var input3 = "17/1/2011 2:00 am";
+			var input4 = "11/Jan/2011 2:00 am";
+			var input5 = "Jan 11, 2011 2:00 am";
+
+			var result1 = function.Execute(FunctionsHelper.CreateArgs(input1), this.ParsingContext);
+			var result2 = function.Execute(FunctionsHelper.CreateArgs(input2), this.ParsingContext);
+			var result3 = function.Execute(FunctionsHelper.CreateArgs(input3), this.ParsingContext);
+			var result4 = function.Execute(FunctionsHelper.CreateArgs(input4), this.ParsingContext);
+			var result5 = function.Execute(FunctionsHelper.CreateArgs(input5), this.ParsingContext);
+
+			Assert.AreEqual(0.083333333, System.Math.Round(result1.ResultNumeric, 9));
+			Assert.AreEqual(0.083333333, System.Math.Round(result2.ResultNumeric, 9));
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result3.Result).Type);
+			Assert.AreEqual(0.083333333, System.Math.Round(result4.ResultNumeric, 9));
+			Assert.AreEqual(0.083333333, System.Math.Round(result5.ResultNumeric, 9));
+		}
+
+		[TestMethod]
+		public void TimeValueIsNormal12HourClock()
+		{
+			var function = new TimeValue();
+
+			var input1 = "00:00 am";
+			var input2 = "00:01 am";
+			var input3 = "12:00 pm";
+			var input4 = "12:00 am";
+			var input6 = "1:00 pm";
+			var input7 = "13:00";
+
+			var result1 = function.Execute(FunctionsHelper.CreateArgs(input1), this.ParsingContext);
+			var result2 = function.Execute(FunctionsHelper.CreateArgs(input2), this.ParsingContext);
+			var result3 = function.Execute(FunctionsHelper.CreateArgs(input3), this.ParsingContext);
+			var result4 = function.Execute(FunctionsHelper.CreateArgs(input4), this.ParsingContext);
+			var result6 = function.Execute(FunctionsHelper.CreateArgs(input6), this.ParsingContext);
+			var result7 = function.Execute(FunctionsHelper.CreateArgs(input7), this.ParsingContext);
+
+			Assert.AreEqual(0.0, result1.ResultNumeric);
+			Assert.AreEqual(0.000694444, System.Math.Round(result2.ResultNumeric, 9));
+			Assert.AreEqual(0.5, result3.ResultNumeric);
+			Assert.AreEqual(0.0, result4.ResultNumeric);
+			Assert.AreEqual(0.541666667, System.Math.Round(result6.ResultNumeric, 9));
+			Assert.AreEqual(result6.ResultNumeric, result7.ResultNumeric);
+
+
+		}
+
+		[TestMethod]
+		public void TimeValueEPPisModdingTimesOver12Pm()
+		{
+			var function = new TimeValue();
+
+			var input1 = "13:00 pm";
+			var input2 = "1:00 pm";
+			var input3 = "16:00 pm";
+			var input4 = "4:00 pm";
+
+			var result1 = function.Execute(FunctionsHelper.CreateArgs(input1), this.ParsingContext);
+			var result2 = function.Execute(FunctionsHelper.CreateArgs(input2), this.ParsingContext);
+			var result3 = function.Execute(FunctionsHelper.CreateArgs(input3), this.ParsingContext);
+			var result4 = function.Execute(FunctionsHelper.CreateArgs(input4), this.ParsingContext);
+
+			Assert.AreEqual(result1.ResultNumeric, result2.ResultNumeric);
+			Assert.AreEqual(result3.ResultNumeric, result4.ResultNumeric);
+			
+		}
+
+		[TestMethod]
+		public void TimeValueTestMilitaryTimeAndNormalTimeComparisions()
+		{
+			var function = new TimeValue();
+
+			var input1 = "16:30";
+			var input2 = "4:30 pm";
+			var input3 = "02:30";
+			var input4 = "2:30 am";
+
+			var result1 = function.Execute(FunctionsHelper.CreateArgs(input1), this.ParsingContext);
+			var result2 = function.Execute(FunctionsHelper.CreateArgs(input2), this.ParsingContext);
+			var result3 = function.Execute(FunctionsHelper.CreateArgs(input3), this.ParsingContext);
+			var result4 = function.Execute(FunctionsHelper.CreateArgs(input4), this.ParsingContext);
+
+			Assert.AreEqual(result1.ResultNumeric,result2.ResultNumeric);
+			Assert.AreEqual(result3.ResultNumeric,result4.ResultNumeric);
+			
+		}
+
+
+
+
+
 		#endregion
 	}
 }
