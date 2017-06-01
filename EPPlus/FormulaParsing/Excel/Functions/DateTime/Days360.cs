67223f3e2ced13ff7b5ff81f47c36d2542b2eeca
Nicole Kister <kister19@up.edu>
2017-06-01 11:51:46 -0700

    Bugs/700 add tests for days306 function (#35)
    * Adding unit tests for the Days360 Function.
    
    * Added all unit tests for the Days360 Function
    
    * Fixed a bug in BoolArgumentParser and commented Days360.
    
    * Added more unit tests for the Days360 function.
    
    * Added support for a few more test cases.
    
    * Brought the Days360 and Days360Tests up to coding standard.

--- EPPlus/FormulaParsing/Excel/Functions/DateTime/Days360.cs
+++ EPPlus/FormulaParsing/Excel/Functions/DateTime/Days360.cs
@@ -2,9 +2,13 @@
 using System.Globalization;
 using System.Linq;
 using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+using OfficeOpenXml.Utils;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 {
+	/// <summary>
+	/// This class contains the formula for computing the number of dates between two given dates. 
+	/// </summary>
 	public class Days360 : ExcelFunction
 	{
 		private enum Days360Calctype
@@ -13,6 +17,12 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 			Us
 		}
 		#region Public ExcelFunction Overrides
+		/// <summary>
+		/// Execute returns the number of days between two user specified dates using the US or European date standards.
+		/// </summary>
+		/// <param name="arguments">The user specified dates and method of calendar they want to use. </param>
+		/// <param name="context">Not used, but needed to override the method.</param>
+		/// <returns></returns>
 		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
 		{
 			if (this.ValidateArguments(arguments, 2) == false)
@@ -20,9 +30,24 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 			if (!this.TryGetArgumentDateValueAtIndex(arguments, 0, out System.DateTime dt1) || !this.TryGetArgumentDateValueAtIndex(arguments, 1, out System.DateTime dt2))
 				return new CompileResult(eErrorType.Value);
 			var calcType = Days360Calctype.Us;
+
 			if (arguments.Count() > 2)
 			{
-				var european = ArgToBool(arguments, 2);
+				var european = false;
+				if (arguments.ElementAt(2).Value is string && ConvertUtil.TryParseNumericString(arguments.ElementAt(2).Value, out double val))
+					return new CompileResult(eErrorType.Value);
+				if(arguments.ElementAt(2).Value is System.DateTime)
+				{
+					european = true;
+				}
+				else
+				{
+					european = this.ArgToBool(arguments, 2);
+					if (arguments.ElementAt(2).Value is int intval && intval > 0)
+						european = true;
+				}
+				
+
 				if (european) calcType = Days360Calctype.European;
 			}
 
@@ -65,11 +90,18 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.DateTime
 				}
 			}
 			var result = (endYear * 12 * 30 + endMonth * 30 + endDay) - (startYear * 12 * 30 + startMonth * 30 + startDay);
-			return CreateResult(result, DataType.Integer);
+			return this.CreateResult(result, DataType.Integer);
 		}
 		#endregion
 
 		#region Private Methods
+		/// <summary>
+		/// Takes a list of arguments and tries to get a date value out of a specified value from the argument list. 
+		/// </summary>
+		/// <param name="arguments">The list of arguments.</param>
+		/// <param name="index">The location of the value we want to try to get a date value from.</param>
+		/// <param name="date">The output date value.</param>
+		/// <returns>True and the date value if we can get a date value from the argument at the specified index.</returns>
 		private bool TryGetArgumentDateValueAtIndex(IEnumerable<FunctionArgument> arguments, int index, out System.DateTime date)
 		{
 			try
