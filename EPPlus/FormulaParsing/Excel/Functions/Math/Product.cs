a631395e03c7a77f7296c26aa77c6f72f08c2711
Nicole Kister <nicolek@jetreports.com>
2017-06-06 11:32:17 -0700

    Completed support for the Product Function. Added necessary comments.

--- EPPlus/FormulaParsing/Excel/Functions/Math/Product.cs
+++ EPPlus/FormulaParsing/Excel/Functions/Math/Product.cs
@@ -1,27 +1,33 @@
-﻿/* Copyright (C) 2011  Jan Källman
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+﻿/*******************************************************************************
+* You may amend and distribute as you like, but don't remove this header!
+*
+* EPPlus provides server-side generation of Excel 2007/2010 spreadsheets.
+* See http://www.codeplex.com/EPPlus for details.
+*
+* Copyright (C) 2011-2017 Jan Källman, Matt Delaney, and others as noted in the source history.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- * See the GNU Lesser General Public License for more details.
- *
- * The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
- * If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
- *
- * All code and executables are provided "as is" with no warranty either express or implied. 
- * The author accepts no liability for any damage or loss of business that this product may cause.
- *
- * Code change notes:
- * 
- * Author							Change						Date
- *******************************************************************************
- * Mats Alm   		                Added		                2013-12-03
- *******************************************************************************/
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU Lesser General Public License for more details.
+*
+* The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+* If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+*
+* All code and executables are provided "as is" with no warranty either express or implied. 
+* The author accepts no liability for any damage or loss of business that this product may cause.
+*
+*  * Author							Change						Date
+* *******************************************************************************
+* * Mats Alm   		                Added		                2013-12-03
+* *******************************************************************************
+* For code change notes, see the source control history.
+*******************************************************************************/
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,21 +37,32 @@ using OfficeOpenXml.Utils;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
 {
+	/// <summary>
+	/// This class contains the formula for the Product Function in Excel.
+	/// </summary>
 	public class Product : HiddenValuesHandlingFunction
 	{
+		/// <summary>
+		/// Calculates the produce of the user specified arguments. 
+		/// </summary>
+		/// <param name="arguments">The user specified arguments to multiply.</param>
+		/// <param name="context">Not used, but needed to override the method.</param>
+		/// <returns>The product of the arguments given.</returns>
 		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
 		{
 			if (ValidateArguments(arguments, 1) == false)
 				return new CompileResult(eErrorType.Value);
 			var result = 0d;
 			var index = 0;
+			double valAsDouble;
 			foreach (var val in arguments)
 			{
 				if (val.Value is int || val.Value is double) { }
 				else if (val.IsExcelRange) { }
 				else if (val.Value is null) { }
-				else if (ConvertUtil.TryParseNumericString(val.Value, out double r)) { }
-				else if (ConvertUtil.TryParseDateObjectToOADate(val.Value, out double e)) { }
+				else if (ConvertUtil.TryParseNumericString(val.Value, out valAsDouble)) { }
+				else if (ConvertUtil.TryParseDateObjectToOADate(val.Value, out valAsDouble)) { }
+				else if (val.Value is IEnumerable<FunctionArgument>) { }
 				else
 					return new CompileResult(eErrorType.Value);
 			}
@@ -72,10 +89,19 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
 						return current;
 					}
 					var obj = arg.Value;
+					double argAsDouble;
 					if (obj != null && IsNumeric(obj))
 					{
 						var val = Convert.ToDouble(obj);
 						current *= val;
+					}
+					else if (ConvertUtil.TryParseNumericString(obj, out argAsDouble ))
+					{
+						return current * argAsDouble;
+					}
+					else if (ConvertUtil.TryParseDateObjectToOADate(obj, out argAsDouble))
+					{
+						return current * argAsDouble;
 					}
 					return current;
 				});
@@ -86,11 +112,26 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
 				return new CompileResult(eErrorType.Value);
 			}
 		}
-
+		/// <summary>
+		/// CalculateFirstItem checks the type of the first argument in the list and sets the first value for multiplication accordingly.
+		/// </summary>
+		/// <param name="arguments">The user specified list of arguments to be multiplied.</param>
+		/// <param name="index">The index of the element to check.</param>
+		/// <param name="context">The context in which the method is being called.</param>
+		/// <returns>The first value in the arguments list as a double.</returns>
 		private double CalculateFirstItem(IEnumerable<FunctionArgument> arguments, int index, ParsingContext context)
 		{
 			var element = arguments.ElementAt(index);
 			var argList = new List<FunctionArgument> { element };
+			double argAsDouble;
+			if(ConvertUtil.TryParseNumericString(arguments.ElementAt(0).Value, out argAsDouble))
+			{
+				return argAsDouble;
+			}
+			else if (ConvertUtil.TryParseDateObjectToOADate(arguments.ElementAt(0).Value, out argAsDouble))
+			{
+				return argAsDouble;
+			}
 			var valueList = ArgsToDoubleEnumerable(false, false, argList, context);
 			var result = 1d;
 			foreach (var value in valueList)
--- EPPlusTest/FormulaParsing/Excel/OperatorsTests.cs
+++ EPPlusTest/FormulaParsing/Excel/OperatorsTests.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿/*******************************************************************************
+* You may amend and distribute as you like, but don't remove this header!
+*
+* EPPlus provides server-side generation of Excel 2007/2010 spreadsheets.
+* See http://www.codeplex.com/EPPlus for details.
+*
+* Copyright (C) 2011-2017 Jan Källman, Matt Delaney, and others as noted in the source history.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU Lesser General Public License for more details.
+*
+* The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+* If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+*
+* All code and executables are provided "as is" with no warranty either express or implied. 
+* The author accepts no liability for any damage or loss of business that this product may cause.
+*
+* For code change notes, see the source control history.
+*******************************************************************************/
+using System;
 using System.Globalization;
 using System.IO;
 using System.Threading;
@@ -315,23 +341,6 @@ namespace EPPlusTest.Excel
 		{
 			var result = Operator.Multiply.Apply(new CompileResult(2, DataType.Integer), new CompileResult("5/1/2017", DataType.String));
 			Assert.AreEqual(85712d, result.Result);
-		}
-
-		[TestMethod]
-		public void OperatorMultiplyWithTwoRangesAsInputReturnsPoundValue()
-		{
-			using (var package = new ExcelPackage())
-			{
-				var ws = package.Workbook.Worksheets.Add("Sheet1");
-				ws.Cells["B1"].Value = 1;
-				ws.Cells["B2"].Value = 2;
-				ws.Cells["B3"].Value = 3;
-				ws.Cells["B4"].Value = 4;
-				ws.Cells["B5"].Value = 5;
-				ws.Cells["B6"].Formula = "B1*B2:B4";
-				ws.Calculate();
-				Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)ws.Cells["B6"].Value).Type);
-			}
 		}
 
 		[TestMethod]
