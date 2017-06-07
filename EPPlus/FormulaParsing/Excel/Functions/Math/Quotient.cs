82e46d98294177c4674ff05e47ed97d4400e32de
Nicole Kister <nicolek@jetreports.com>
2017-06-07 08:45:52 -0700

    Added unit test for the Quotient Function.

--- EPPlus/FormulaParsing/Excel/Functions/Math/Quotient.cs
+++ EPPlus/FormulaParsing/Excel/Functions/Math/Quotient.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+using OfficeOpenXml.Utils;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
 {
@@ -9,9 +11,22 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
 		{
 			if (ValidateArguments(arguments, 2) == false)
 				return new CompileResult(eErrorType.Value);
+			var numeratorCandidate = arguments.ElementAt(0).Value;
+			var denominatorCandidate = arguments.ElementAt(1).Value;
+			double candidateAsDouble;
+
+			if (numeratorCandidate == null ||denominatorCandidate == null)
+				return new CompileResult(eErrorType.NA);
+			if (!ConvertUtil.TryParseNumericString(numeratorCandidate, out candidateAsDouble))
+				if (!ConvertUtil.TryParseDateObjectToOADate(numeratorCandidate, out candidateAsDouble))
+					return new CompileResult(eErrorType.Value);
+			if (!ConvertUtil.TryParseNumericString(denominatorCandidate, out candidateAsDouble))
+				if (!ConvertUtil.TryParseDateObjectToOADate(denominatorCandidate, out candidateAsDouble))
+					return new CompileResult(eErrorType.Value);
+
 			var num = ArgToDecimal(arguments, 0);
 			var denom = ArgToDecimal(arguments, 1);
-			if ((int)denom == 0)
+			if (denom == 0.0)
 				return new CompileResult(eErrorType.Div0);
 			var result = (int)(num / denom);
 			return CreateResult(result, DataType.Integer);
