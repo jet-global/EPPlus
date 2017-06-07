06e696a463d8d7ceb3dbe5a5642fb8df7e3c06d2
Nicole Kister <kister19@up.edu>
2017-06-07 15:28:22 -0700

    Bugs/805 support quotient function (#51)
    * Added unit test for the Quotient Function.
    
    * Added comments.
    
    * Added appropriate header comments.
    
    * Added appropriate syntax.

---
+++ EPPlusTest/FormulaParsing/Excel/Functions/Math/QuotientTests.cs
@@ -0,0 +1,150 @@
+﻿/*******************************************************************************
+* You may amend and distribute as you like, but don't remove this header!
+*
+* EPPlus provides server-side generation of Excel 2007/2010 spreadsheets.
+* See http://www.codeplex.com/EPPlus for details.
+*
+* Copyright (C) 2011-2017 Jan Källman, Matt Delaney, and others as noted in the source history.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU Lesser General Public License for more details.
+*
+* The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+* If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+*
+* All code and executables are provided "as is" with no warranty either express or implied. 
+* The author accepts no liability for any damage or loss of business that this product may cause.
+*
+* For code change notes, see the source control history.
+*******************************************************************************/
+using System;
+using EPPlusTest.FormulaParsing.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using OfficeOpenXml;
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.Math
+{
+	[TestClass]
+	public class QuotientTests : MathFunctionsTestBase
+	{
+		#region Quotient Function (Execute) Tests
+		[TestMethod]
+		public void QuotientWithTwoPositiveIntegersReturnsCorrectValue()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs(10, 5), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+
+		[TestMethod]
+		public void QuotientWithDoubleArgumentsReturnsCorrectValue()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs(6.3, 3.3), this.ParsingContext);
+			Assert.AreEqual(1, result.Result);
+		}
+
+		[TestMethod]
+		public void QuotientWithPositiveAndNegativeIntegerReturnsCorrectValue()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-10, 3), this.ParsingContext);
+			Assert.AreEqual(-3, result.Result);
+		}
+
+		[TestMethod]
+		public void QuotientWithDivisionByZeroReturnsDivZeroError()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs(1, 0), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Div0, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void QuotientWithZeroAsNumeratorReturnsCorrectValue()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs(0, 1), this.ParsingContext);
+			Assert.AreEqual(0, result.Result);
+		}
+
+		[TestMethod]
+		public void QuotientWithNullFirstArgumentReturnsPoundNA()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs(null, 2), this.ParsingContext);
+			Assert.AreEqual(eErrorType.NA, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void QuotientWithNullSecondArgumentReturnsPoundNA()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs(2, null), this.ParsingContext);
+			Assert.AreEqual(eErrorType.NA, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void QuotientWithNoArgumentsReturnsPoundValue()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs(), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void QuotientWithDateFunctionArgumentsReturnsCorrectValue()
+		{
+			var function = new Quotient();
+			var firstArgAsDate = new DateTime(2017, 5, 1);
+			var secondArgAsDate = new DateTime(2017, 6, 1);
+			var result = function.Execute(FunctionsHelper.CreateArgs(firstArgAsDate, secondArgAsDate), this.ParsingContext);
+			Assert.AreEqual(0, result.Result);
+		}
+
+		[TestMethod]
+		public void QuotientWithDatesAsStringArgumentsReturnsCorrectValue()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs("5/1/2017", "6/1/2017"), this.ParsingContext);
+			Assert.AreEqual(0, result.Result);
+		}
+
+		[TestMethod]
+		public void QuotientWithFractionsReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var ws = package.Workbook.Worksheets.Add("Sheet1");
+				ws.Cells["B1"].Formula = "QUOTIENT((2/3),(1/3))";
+				ws.Calculate();
+				Assert.AreEqual(2, ws.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void QuotientWithStringArgumentsReturnsPoundValue()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs("string", "string"), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void QuotientWithNumericStringsReturnsCorrectValue()
+		{
+			var function = new Quotient();
+			var result = function.Execute(FunctionsHelper.CreateArgs("10", "5"), this.ParsingContext);
+			Assert.AreEqual(2, result.Result);
+		}
+		#endregion
+	}
+}
