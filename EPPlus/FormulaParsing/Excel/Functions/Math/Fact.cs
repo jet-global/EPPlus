e960818021cdf9cbe0e4dfd23aefa2c7f129bf36
colewfrench <colewfrench@users.noreply.github.com>
2017-06-12 09:18:13 -0700

    Add comments to the FACT function.

--- EPPlus/FormulaParsing/Excel/Functions/Math/Fact.cs
+++ EPPlus/FormulaParsing/Excel/Functions/Math/Fact.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections.Generic;
+﻿/*******************************************************************************
+* You may amend and distribute as you like, but don't remove this header!
+*
+* EPPlus provides server-side generation of Excel 2007/2010 spreadsheets.
+* See http://www.codeplex.com/EPPlus for details.
+*
+* Copyright (C) 2011-2017 Jan Källman, Matt Delaney, and others as noted in the source history.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU Lesser General Public License for more details.
+*
+* The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+* If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+*
+* All code and executables are provided "as is" with no warranty either express or implied. 
+* The author accepts no liability for any damage or loss of business that this product may cause.
+*
+* For code change notes, see the source control history.
+*******************************************************************************/
+using System.Collections.Generic;
 using System.Linq;
 using OfficeOpenXml.FormulaParsing.ExpressionGraph;
 using OfficeOpenXml.Utils;
 
 namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
 {
+	/// <summary>
+	/// Returns the factorial of a number.
+	/// </summary>
 	public class Fact : ExcelFunction
 	{
+		/// <summary>
+		/// Returns the factorial of a number.
+		/// </summary>
+		/// <param name="arguments">Contains the number to perform the factorial operation on.</param>
+		/// <param name="context">Unused in the method.</param>
+		/// <returns>Returns the factorial of the given number, or an <see cref="ExcelErrorValue"/> if the given input is invalid.</returns>
 		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
 		{
 			if (this.ValidateArguments(arguments, 1) == false)
 				return new CompileResult(eErrorType.Value);
 			if (arguments.ElementAt(0).ValueIsExcelError)
 				return new CompileResult(arguments.ElementAt(0).ValueAsExcelErrorValue);
+			// Despite its name, this method just turns objects into numbers. Its original purpose was for parsing
+			// any object as if it represented a date, but it's basically the same as ArgToInt, except that it can
+			// parse dates as well.
 			if (!ConvertUtil.TryParseDateObjectToOADate(arguments.ElementAt(0).Value, out double parsedNumberAsDouble))
 				return new CompileResult(eErrorType.Value);
 			var number = (int)parsedNumberAsDouble;
@@ -23,7 +61,7 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
 			{
 				result *= x;
 			}
-			return CreateResult(result, DataType.Integer);
+			return this.CreateResult(result, DataType.Integer);
 		}
 	}
 }
