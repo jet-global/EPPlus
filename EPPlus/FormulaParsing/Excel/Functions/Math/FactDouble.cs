3a524f34c92e8184a402a0aeda0f2d45219c8e09
colewfrench <colewfrench@users.noreply.github.com>
2017-06-12 13:36:59 -0700

    Bugs/800 provide support for exp function (#59)
    * Add unit tests for the EXP function.
    
    * Changed the EXP function to pass all unit tests.
    
    * Add comments and clean up the unit tests.
    
    * Add unit tests for the FACT function.
    
    * Changed the FACT function to pass all tests.
    
    * Add comments to the FACT function.
    
    * Fixed comment headers on files and created the FACTDOUBLE file and its test files.
    
    * Add unit tests for the FACTDOUBLE function and change the FACTDOUBLE function to pass all tests.
    
    * Refactor the FACTDOUBLE function and add comments.
    
    * Added FACTDOUBLE to the list of functions.
    
    * Apply changes listed in the code review.

---
+++ EPPlus/FormulaParsing/Excel/Functions/Math/FactDouble.cs
@@ -0,0 +1,74 @@
+﻿/*******************************************************************************
+* You may amend and distribute as you like, but don't remove this header!
+*
+* EPPlus provides server-side generation of Excel 2007/2010 spreadsheets.
+* See http://www.codeplex.com/EPPlus for details.
+*
+* Copyright (C) 2011-2017 Jan Källman, Matt Delaney, and others as noted in the source history.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU Lesser General Public License for more details.
+*
+* The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+* If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+*
+* All code and executables are provided "as is" with no warranty either express or implied. 
+* The author accepts no liability for any damage or loss of business that this product may cause.
+*
+* For code change notes, see the source control history.
+*******************************************************************************/
+using System.Collections.Generic;
+using System.Linq;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+using OfficeOpenXml.Utils;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
+{
+	/// <summary>
+	/// Returns the double factorial of a number.
+	/// The double factorial is defined as such:
+	/// Given a number n,
+	///		if n is even, the double factorial of n is equal to n*(n-2)*(n-4)*...*(4)*(2).
+	///		if n is odd, the double factorial of n is equal to n*(n-2)*(n-4)*...*(3)*(1).
+	///		
+	/// Excel documentation:
+	///		https://support.office.com/en-us/article/FACTDOUBLE-function-e67697ac-d214-48eb-b7b7-cce2589ecac8
+	///	
+	/// More information on the double factorial:
+	///		https://en.wikipedia.org/wiki/Double_factorial
+	/// </summary>
+	public class FactDouble : ExcelFunction
+	{
+		/// <summary>
+		/// Returns the double factorial of a number.
+		/// </summary>
+		/// <param name="arguments">Contains the number to perform the double factorial operation on.</param>
+		/// <param name="context">Unused in the method.</param>
+		/// <returns>Returns the double factorial of the given number, or an <see cref="ExcelErrorValue"/> if the input is invalid.</returns>
+		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
+		{
+			if (this.ValidateArguments(arguments, 1) == false)
+				return new CompileResult(eErrorType.Value);
+			if (arguments.ElementAt(0).ValueIsExcelError)
+				return new CompileResult(arguments.ElementAt(0).ValueAsExcelErrorValue);
+			if (!ConvertUtil.TryParseDateObjectToOADate(arguments.ElementAt(0).Value, out double parsedDouble))
+				return new CompileResult(eErrorType.Value);
+			if (parsedDouble < -1)
+				return new CompileResult(eErrorType.Num);
+			var number = (int)parsedDouble;
+			var result = 1d;
+			for (var i = (2 - number % 2); i <= number; i += 2)
+			{
+				result *= i;
+			}
+			return this.CreateResult(result, DataType.Integer);
+		}
+	}
+}
---
+++ EPPlusTest/FormulaParsing/Excel/Functions/Math/FactDoubleTests.cs
@@ -0,0 +1,267 @@
+﻿/*******************************************************************************
+* You may amend and distribute as you like, but don't remove this header!
+*
+* EPPlus provides server-side generation of Excel 2007/2010 spreadsheets.
+* See http://www.codeplex.com/EPPlus for details.
+*
+* Copyright (C) 2011-2017 Jan Källman, Matt Delaney, and others as noted in the source history.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU Lesser General Public License for more details.
+*
+* The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+* If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+*
+* All code and executables are provided "as is" with no warranty either express or implied. 
+* The author accepts no liability for any damage or loss of business that this product may cause.
+*
+* For code change notes, see the source control history.
+*******************************************************************************/
+using EPPlusTest.FormulaParsing.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.Math
+{
+	[TestClass]
+	public class FactDoubleTests : MathFunctionsTestBase
+	{
+		#region FactDouble Tests
+		[TestMethod]
+		public void FactDoubleWithTooFewArgumentsReturnsPoundValue()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs();
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithInputAsOneReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(1);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithInputAsTwoReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(2);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(2d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithInputAsThreeReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(3);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(3d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithPositiveEvenIntegerReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(4);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(8d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithPositiveOddIntegerReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(5);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(15d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithPositiveEvenDoubleReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(4.9);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(8d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithPositiveOddDoubleReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(5.9);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(15d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithInputAsZeroReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(0);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithNegativeFractionReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(-0.5);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithNegativeOneReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(-1);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithNegativeDoubleLessThanNegativeOneReturnsPoundNum()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(-1.5);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithNegativeIntegerLessThenNegativeOneReturnsPoundNum()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(-2);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithPositiveEvenIntegerInStringReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("4");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(8d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithPositiveOddIntegerInStringReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("5");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(15d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithPositiveEvenDoubleInStringReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("4.9");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(8d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithPositiveOddDoubleInStringReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("5.9");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(15d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithZeroInStringReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("0");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithNegativeOneInStringReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("-1");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithNegativeIntegerLessThanNegativeOneInStringReturnsPoundNum()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("-2");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithNegativeDoubleLessThanNegativeOneInStringReturnsPoundNum()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("-2.5");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Num, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithNonNumericStringReturnsPoundValue()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("word");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithEmptyStringReturnsPoundValue()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(string.Empty);
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithDateInStringReturnsCorrectResult()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs("1/1/1900");
+			var result = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void FactDoubleWithErrorValueInputReturnsThatErrorValue()
+		{
+			var func = new FactDouble();
+			var args = FunctionsHelper.CreateArgs(ExcelErrorValue.Create(eErrorType.NA));
+			var resultNA = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.NA, ((ExcelErrorValue)resultNA.Result).Type);
+			args = FunctionsHelper.CreateArgs(ExcelErrorValue.Create(eErrorType.Div0));
+			var resultDiv0 = func.Execute(args, this.ParsingContext);
+			Assert.AreEqual(eErrorType.Div0, ((ExcelErrorValue)resultDiv0.Result).Type);
+		}
+		#endregion
+	}
+}
