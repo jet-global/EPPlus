956aef9d50bb5f71cb44206e0ed8a1e96bedab8a
Nicole Kister <nicolek@jetreports.com>
2017-06-14 11:24:25 -0700

    Adding test suite for the Min Function.

---
+++ EPPlusTest/FormulaParsing/Excel/Functions/Math/MinTests.cs
@@ -0,0 +1,74 @@
+﻿/*******************************************************************************
+* You may amend and distribute as you like, but don't remove this header!
+*
+* EPPlus provides server-side generation of Excel 2007/2010 spreadsheets.
+* See http://www.codeplex.com/EPPlus for details.
+*
+* Copyright (C) 2011-2017 Jan Källman, Matt Delaney, and others as noted in the source history.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU Lesser General Public License for more details.
+*
+* The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+* If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+*
+* All code and executables are provided "as is" with no warranty either express or implied. 
+* The author accepts no liability for any damage or loss of business that this product may cause.
+*
+* For code change notes, see the source control history.
+*******************************************************************************/
+using System;
+using EPPlusTest.FormulaParsing.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing.Excel;
+using System.Linq;
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.Math
+{
+	[TestClass]
+	public class MinTests : MathFunctionsTestBase
+	{
+		[TestMethod]
+		public void MinWithNoArgumentsReturnsPoundValue()
+		{
+			var function = new Min();
+			var result = function.Execute(FunctionsHelper.CreateArgs(), this.ParsingContext);
+		}
+
+		[TestMethod]
+		public void MinWithReferenceToEmptyCellReturnsZero()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "MIN(A2:A4)";
+				worksheet.Calculate();
+				Assert.AreEqual(0d, worksheet.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void MinWithReferenceToCellWithLogicalValueReturnsCorrectResult()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Value = 5;
+				worksheet.Cells["B2"].Value = 2;
+				worksheet.Cells["B3"].Value = "TRUE";
+				worksheet.Cells["B4"].Formula = "MIN(B1:B3)";
+				worksheet.Calculate();
+				Assert.AreEqual(2d, worksheet.Cells["B4"].Value);
+			}
+		}
+	}
+}
