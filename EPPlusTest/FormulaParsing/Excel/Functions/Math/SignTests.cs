ef97e425778ae971c8e5157218597facfa7aa148
Nicole Kister <nicolek@jetreports.com>
2017-06-20 09:54:30 -0700

    Completed test suite for the Sign Function.

--- EPPlusTest/FormulaParsing/Excel/Functions/Math/SignTests.cs
+++ EPPlusTest/FormulaParsing/Excel/Functions/Math/SignTests.cs
@@ -44,8 +44,8 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.Math
 		{
 			var function = new Sign();
 			var args = FunctionsHelper.CreateArgs(-2);
-			var result = function.Execute(args, this.ParsingContext).Result;
-			Assert.AreEqual(-1d, result);
+			var result = function.Execute(args, this.ParsingContext);
+			Assert.AreEqual(-1d, result.Result);
 		}
 
 		[TestMethod]
@@ -53,8 +53,8 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.Math
 		{
 			var function = new Sign();
 			var args = FunctionsHelper.CreateArgs(2);
-			var result = function.Execute(args, this.ParsingContext).Result;
-			Assert.AreEqual(1d, result);
+			var result = function.Execute(args, this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
 		}
 
 		[TestMethod]
@@ -93,7 +93,117 @@ namespace EPPlusTest.FormulaParsing.Excel.Functions.Math
 		[TestMethod]
 		public void SignWithSubtractionInFunctionCallReturnsCorrectValue()
 		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs(1 - 5), this.ParsingContext);
+			Assert.AreEqual(-1d, result.Result);
+		}
 
+		[TestMethod]
+		public void SignWithMultiplicationInFunctionCallReturnsCorrectValue()
+		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs(8 * 9), this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void SignWithDivisionInFunctionCallReturnsCorrectValue()
+		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs(5 / 4), this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void SignWithDivisonByZeroReturnsPoundDivZero()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "SIGN(5/0)";
+				worksheet.Calculate();
+				Assert.AreEqual(eErrorType.Div0, ((ExcelErrorValue)worksheet.Cells["B1"].Value).Type);
+			}
+		}
+
+		[TestMethod]
+		public void SignWithDateFunctionReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "SIGN(DATE(2017, 5, 7))";
+				worksheet.Calculate();
+				Assert.AreEqual(1d, worksheet.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void SignWithGeneralStringReturnsPoundValue()
+		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs("string"), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void SignWithDatesAsStringsReturnsCorrectValue()
+		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs("5/5/2017"), this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void SignWithTrueBooleanReturnsCorrectValue()
+		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs(true), this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void SignWithFalseBooleanReturnsCorrectValue()
+		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs(false), this.ParsingContext);
+			Assert.AreEqual(0d, result.Result);
+		}
+
+		[TestMethod]
+		public void SignWithZeroInputReutrnsZero()
+		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs(0), this.ParsingContext);
+			Assert.AreEqual(0d, result.Result);
+		}
+
+		[TestMethod]
+		public void SignWithPositiveNumericStringReturnsCorrectValue()
+		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs("3"), this.ParsingContext);
+			Assert.AreEqual(1d, result.Result);
+		}
+
+		[TestMethod]
+		public void SignWithNegativeNumericStringReturnsCorrectValue()
+		{
+			var function = new Sign();
+			var result = function.Execute(FunctionsHelper.CreateArgs("-5"), this.ParsingContext);
+			Assert.AreEqual(-1d, result.Result);
+		}
+
+		[TestMethod]
+		public void SignWithReferenceToEmptyCellReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "SIGN(A2)";
+				worksheet.Calculate();
+				Assert.AreEqual(0d, worksheet.Cells["B1"].Value);
+			}
 		}
 		#endregion
 	}
