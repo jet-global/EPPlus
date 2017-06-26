d23bf281c3c551f373dc14cbefc95138733c6168
Nicole Kister <kister19@up.edu>
2017-06-26 13:52:09 -0700

    Bugs/794 add floor math and ceiling math functions (#81)
    * Adding test suite for the Floor.Math function.
    
    * Providing support to the Floor.Math Function.
    
    * Completed support for the FloorMath Function.
    
    * Created CeilingMath Function class and test suite.
    
    * Adding test cases for the CeilingMath Function.
    
    * Adding tests to the test suite.
    
    * Completed test suite for the CeilingMath Function.
    
    * Completed support for the CeilingMath Function.
    
    * Completed support for the Ceiling.Math Function and the Floor.Math Function.
    
    * Condensed the CeilingMath Function.
    
    * Removed extra white space.
    
    * Completed changes to the CeilingMath Function.

---
+++ EPPlus/FormulaParsing/Excel/Functions/Math/CeilingMath.cs
@@ -0,0 +1,123 @@
+﻿/*******************************************************************************
+* You may amend and distribute as you like, but don't remove this header!
+*
+* EPPlus provides server-side generation of Excel 2007/2010 spreadsheets.
+* See http://www.codeplex.com/EPPlus for details.
+*
+* Copyright (C) 2011-2017 Jan Källman, Matt Delaney, and others as noted in the source history.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU Lesser General Public License for more details.
+*
+* The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+* If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+*
+* All code and executables are provided "as is" with no warranty either express or implied. 
+* The author accepts no liability for any damage or loss of business that this product may cause.
+*
+* For code change notes, see the source control history.
+*******************************************************************************/
+using System.Collections.Generic;
+using System.Linq;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+using OfficeOpenXml.Utils;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Math
+{
+	/// <summary>
+	/// This class contains the formula for the CEILING.MATH Excel Function. 
+	/// </summary>
+	public class CeilingMath : ExcelFunction
+	{
+		/// <summary>
+		/// Takes the first user argument and rounds it up based on the optional second and third arguments.
+		/// </summary>
+		/// <param name="arguments">The first argument is the number to be rounded up, the second is the significance value to round the
+		/// number to, and the third argument is the mode that determines if the number is rounded closer to zero or not (for negative 
+		/// numbers only).</param>
+		/// <param name="context">Not used, but needed to override the method.</param>
+		/// <returns>The first argument rounded up based on the specifications of the second and third 
+		/// optional user arguments.</returns>
+		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
+		{
+			double divisionResult;
+			int multiple;
+			bool exactChange;
+
+			if (this.ArgumentsAreValid(arguments, 1, out eErrorType argumentError) == false)
+				return new CompileResult(argumentError);
+			var numberCandidate = arguments.ElementAt(0).Value;
+			var significanceCandidate = arguments.ElementAt(1).Value;
+			if (numberCandidate == null)
+				return this.CreateResult(0d, DataType.Decimal);
+			if (!ConvertUtil.TryParseDateObjectToOADate(numberCandidate, out double number))
+				return new CompileResult(eErrorType.Value);
+			if (significanceCandidate == null)
+				return this.CreateResult(System.Math.Ceiling(number), DataType.Decimal);
+			if (!ConvertUtil.TryParseDateObjectToOADate(significanceCandidate, out double significance))
+				return new CompileResult(eErrorType.Value);
+			if (significance < 0)
+				significance = -1 * significance;
+			if (arguments.Count() > 2)
+			{
+				var modeCandidate = arguments.ElementAt(2).Value ?? 0;
+				if (!ConvertUtil.TryParseDateObjectToOADate(modeCandidate, out double mode))
+					return new CompileResult(eErrorType.Value);
+				if (mode == 0 || number > 0)
+				{
+					if (significance < 1 && significance > 0)
+					{
+						var floor = System.Math.Floor(number);
+						var rest = number - floor;
+						var nSign = (int)(rest / significance) + 1;
+						return this.CreateResult(floor + (nSign * significance), DataType.Decimal);
+					}
+					else if (significance == 1)
+						return this.CreateResult(System.Math.Ceiling(number), DataType.Decimal);
+					else if (number % significance == 0)
+						return this.CreateResult(number, DataType.Decimal);
+					else if (number < 0 && significance > 0)
+					{
+						var modNum = -1 * (number % significance);
+						var result = number + modNum;
+						return this.CreateResult(result, DataType.Decimal);
+					}
+					else
+					{
+						var result = number - (number % significance) + significance;
+						return this.CreateResult(result, DataType.Decimal);
+					}
+				}
+				divisionResult = number / significance;
+				multiple = (int)divisionResult;
+				exactChange = divisionResult == multiple;
+				if (exactChange)
+					return this.CreateResult(number, DataType.Decimal);
+				else if (significance > 0 && number < 0)
+					return this.CreateResult((multiple - 1) * significance, DataType.Decimal);
+				else if (number < 0)
+					return this.CreateResult((multiple + 1) * significance, DataType.Decimal);
+				else
+					return this.CreateResult(multiple * significance, DataType.Decimal);
+			}
+			else if (significance == 1)
+				return this.CreateResult(System.Math.Ceiling(number), DataType.Decimal);
+			else if (significance == 0 || number == 0)
+				return this.CreateResult(0d, DataType.Decimal);
+			else if (number % significance == 0)
+				return this.CreateResult(number, DataType.Decimal);
+			else
+			{
+				var result = number - (number % significance) + significance;
+				return this.CreateResult(result, DataType.Decimal);
+			}
+		}
+	}
+}
---
+++ EPPlusTest/FormulaParsing/Excel/Functions/Math/FloorMathTests.cs
@@ -0,0 +1,333 @@
+﻿/*******************************************************************************
+* You may amend and distribute as you like, but don't remove this header!
+*
+* EPPlus provides server-side generation of Excel 2007/2010 spreadsheets.
+* See http://www.codeplex.com/EPPlus for details.
+*
+* Copyright (C) 2011-2017 Jan Källman, Matt Delaney, and others as noted in the source history.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU Lesser General Public License for more details.
+*
+* The GNU Lesser General Public License can be viewed at http://www.opensource.org/licenses/lgpl-license.php
+* If you unfamiliar with this license or have questions about it, here is an http://www.gnu.org/licenses/gpl-faq.html
+*
+* All code and executables are provided "as is" with no warranty either express or implied. 
+* The author accepts no liability for any damage or loss of business that this product may cause.
+*
+* For code change notes, see the source control history.
+*******************************************************************************/
+using System;
+using EPPlusTest.FormulaParsing.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using OfficeOpenXml;
+
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.Math
+{
+	[TestClass]
+	public class FloorMathTests : MathFunctionsTestBase
+	{
+		[TestMethod]
+		public void FloorMathWithNoInputsReturnsPoundValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+		
+		[TestMethod]
+		public void FloorMathWithOneInputReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "FLOOR.MATH(15.67, , )";
+				worksheet.Calculate();
+				Assert.AreEqual(15d, worksheet.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void FloorMathWithTwoInputsReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "FLOOR.MATH(14.56, 3, )";
+				worksheet.Calculate();
+				Assert.AreEqual(12d, worksheet.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void FloorMathWithAllThreeInputsReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(14.56, 4, 6), this.ParsingContext);
+			Assert.AreEqual(12d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithNegativeNumberPositiveSigReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-15.36, 2), this.ParsingContext);
+			Assert.AreEqual(-16d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithNegativeNumberNegativeSigReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-15.6, -7), this.ParsingContext);
+			Assert.AreEqual(-21d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithPositiveNumberNegativeSigReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(15.7, -2), this.ParsingContext);
+			Assert.AreEqual(14d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithModeValueReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-5.5, 2, -1), this.ParsingContext);
+			Assert.AreEqual(-4d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithNegativeNumberAndNoModeValueReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-5.5, 2), this.ParsingContext);
+			Assert.AreEqual(-6d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithDecimalSignificanceReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(45.67, 2.34), this.ParsingContext);
+			Assert.AreEqual(44.46d, (double)result.Result, 0.000001);
+		}
+
+		[TestMethod]
+		public void FloorMathWithZeroModeReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-5.5, 2, 0), this.ParsingContext);
+			Assert.AreEqual(-6d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithZeroSignificanceReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(56.7, 0), this.ParsingContext);
+			Assert.AreEqual(0d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithGeneralStringNumberReturnsPoundValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs("String", 3, 5), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FloorMathWithDateFunctionNumberReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "FLOOR.MATH(DATE(2017, 6, 14), 4)";
+				worksheet.Calculate();
+				Assert.AreEqual(42900d, worksheet.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void FloorMathWithDateAsStringNumberReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs("5/5/2017", 4), this.ParsingContext);
+			Assert.AreEqual(42860d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithBooleanNumberInputReturnsZero()
+		{
+			var function = new FloorMath();
+			var booleanTrue = function.Execute(FunctionsHelper.CreateArgs(true, 5), this.ParsingContext);
+			var booleanFalse = function.Execute(FunctionsHelper.CreateArgs(false, 3), this.ParsingContext);
+			Assert.AreEqual(0d, booleanTrue.Result);
+			Assert.AreEqual(0d, booleanFalse.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithNumericStringNumberReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs("5.67", 3), this.ParsingContext);
+			Assert.AreEqual(3d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithGeneralStringSignificanceReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(34.5, "String"), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FloorMathWithDateFunctionSignificanceReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "FLOOR.MATH(34.5, DATE(2017, 6, 15))";
+				worksheet.Calculate();
+				Assert.AreEqual(0d, worksheet.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void FloorMathWithDateAsStringSignificanceReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(45548564.6, "5/5/2017"), this.ParsingContext);
+			Assert.AreEqual(45517320d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithBooleanSignificanceReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var booleanTrue = function.Execute(FunctionsHelper.CreateArgs(45.6, true), this.ParsingContext);
+			var booleanFalse = function.Execute(FunctionsHelper.CreateArgs(45.6, false), this.ParsingContext);
+			Assert.AreEqual(45d, booleanTrue.Result);
+			Assert.AreEqual(0d, booleanFalse.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithNumericStringSignificanceReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(4.67, "2"), this.ParsingContext);
+			Assert.AreEqual(4d, result.Result);
+		}
+
+
+
+		// Below are tests that deal with mode input.
+
+		[TestMethod]
+		public void FloorMathWithGeneralStringModeInputReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(34.5, 3, "string"), this.ParsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void FloorMathWithDateFunctionModeInputReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "FLOOR.MATH(-45.5, 3, DATE(2017, 6, 15))";
+				worksheet.Calculate();
+				Assert.AreEqual(-45d, worksheet.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void FloorMathWithDateAsStringModeInputReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-45.6, 3, "5/5/2017"), this.ParsingContext);
+			Assert.AreEqual(-45d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithBooleanModeInputReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var booleanTrue = function.Execute(FunctionsHelper.CreateArgs(-45.7, 3, true), this.ParsingContext);
+			var booleanFalse = function.Execute(FunctionsHelper.CreateArgs(-45.7, 3, false), this.ParsingContext);
+			Assert.AreEqual(-45d, booleanTrue.Result);
+			Assert.AreEqual(-48d, booleanFalse.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithNumericStringModeInputReturnsCorrectValue()
+		{
+			var function = new FloorMath();
+			var result = function.Execute(FunctionsHelper.CreateArgs(-45.7, 4, "2"), this.ParsingContext);
+			Assert.AreEqual(-44d, result.Result);
+		}
+
+		[TestMethod]
+		public void FloorMathWithFirstAndThirdInputsReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "FLOOR.MATH(45.6, ,7)";
+				worksheet.Calculate();
+				Assert.AreEqual(45d, worksheet.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void FloorMathWithOnlySecondAndThirdInputsReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "FLOOR.MATH(, 2, 3)";
+				worksheet.Calculate();
+				Assert.AreEqual(0d, worksheet.Cells["B1"].Value);
+			}
+		}
+
+		[TestMethod]
+		public void FloorMathWithOnlyThirdInputReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "FLOOR.MATH(, , 2)";
+				worksheet.Calculate();
+				Assert.AreEqual(0d, worksheet.Cells["B1"].Value);
+			}
+		}
+		
+		[TestMethod]
+		public void FloorMathWithOnlySecondInputReturnsCorrectValue()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["B1"].Formula = "FLOOR.MATH(, 2, )";
+				worksheet.Calculate();
+				Assert.AreEqual(0d, worksheet.Cells["B1"].Value);
+			}
+		}
+	}
+}
