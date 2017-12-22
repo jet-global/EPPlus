c676ae951b957cb84a0278581fd7aa5cdf01cb58
Zachary Faltersack <zacharyf@jetreports.com>
2017-12-21 16:44:02 -0800

    Basic pass.

---
+++ EPPlus/ZCellStore.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeOpenXml
+{
+	internal class ZCellStore<T> : ICellStore<T>
+	{
+		private class Sheet
+		{
+			// contains 128 pages of columns
+			public ColumnsPage[] Pages { get; } = new ColumnsPage[ZCellStore<T>.ColumnPageSize];
+		}
+
+		internal class ColumnsPage
+		{
+			// contains 128 columns from a page 
+			public Column[] Columns { get; } = new Column[ZCellStore<T>.ColumnPageSize];
+		}
+
+		internal class Column
+		{
+			// contains 1024 pages of rows
+			public RowsPage[] Pages { get; } = new RowsPage[ZCellStore<T>.RowPageSize];
+		}
+
+		internal class RowsPage
+		{
+			// Contains 1024 rows from a page in the column
+			public DataWrapper[] Data { get; } = new DataWrapper[ZCellStore<T>.RowPageSize];
+		}
+
+		internal class DataWrapper // TODO -- Consider making this a struct
+		{
+			public T Item { get; set; }
+		}
+
+		#region Constants
+		private const int RowPageSize = 1024; // The number of rows in a row page
+		private const int ColumnPageSize = 128; // The number of columns in a column page
+		private const int RowMask = 1023; // 111111111 ( 9 1's )
+		private const int ColumnMask = 127; // 111111 ( 6 1's)
+		private const int RowShift = 10; // 2 ^ 10 = 1024 so right-shifting by 10 bits divides by 1024
+		private const int ColumnShift = 7; // 2 ^ 7 = 128 so right-shifting by 7 bits divides by 128
+		#endregion
+
+		private Sheet SheetInternal { get; } = new Sheet();
+
+		private int MaxUsedColumn = 0;
+		private int MaxUsedRow = 0;
+
+		#region ICellStore<T> Methods
+		public T GetValue(int row, int column)
+		{
+			this.Exists(row, column, out T value);
+			return value;
+		}
+
+		public void SetValue(int row, int column, T value)
+		{
+			if (ZCellStore<T>.GetRowCoordinates(row, out int rowPage, out int rowPageIndex) &&
+					ZCellStore<T>.GetColumnCoordinates(column, out int columnPage, out int columnPageIndex))
+			{
+				var columnObj = this.GetColumn(columnPage, columnPageIndex, true);
+				var cell = this.GetRow(columnObj, rowPage, rowPageIndex, true);
+				cell.Item = value;
+			}
+		}
+
+		public bool Exists(int row, int column, out T value)
+		{
+			value = default(T);
+			if (ZCellStore<T>.GetRowCoordinates(row, out int rowPage, out int rowPageIndex) &&
+					ZCellStore<T>.GetColumnCoordinates(column, out int columnPage, out int columnPageIndex))
+			{
+				var columnObj = this.GetColumn(columnPage, columnPageIndex, false);
+				if (null == columnObj)
+					return false;
+				var cell = this.GetRow(columnObj, rowPage, rowPageIndex, false);
+				if (null == cell)
+					return false;
+				value = cell.Item;
+				return true;
+			}
+			return false;
+		}
+
+		public bool NextCell(ref int row, ref int column)
+		{
+			while (row <= this.MaxUsedRow)
+			{
+				while (++column <= this.MaxUsedColumn)
+				{
+					if (ZCellStore<T>.GetColumnCoordinates(column, out int columnPage, out int columnPageIndex))
+					{
+						var columnObj = this.GetColumn(columnPage, columnPageIndex, false);
+						if (null != columnObj)
+						{
+							if (ZCellStore<T>.GetRowCoordinates(row, out int rowPage, out int rowPageIndex))
+							{
+								var cell = this.GetRow(columnObj, rowPage, rowPageIndex, false);
+								if (cell != null)
+									return true;
+							}
+						}
+					}
+				}
+				column = -1;
+				++row;
+			}
+			return false;
+		}
+
+		public bool PrevCell(ref int row, ref int column)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(int fromRow, int fromCol, int rows, int columns)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(int fromRow, int fromCol, int rows, int columns, bool shift)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Exists(int row, int column)
+		{
+			return this.Exists(row, column, out T value);
+		}
+
+		public void Clear(int _fromRow, int _fromCol, int toRow, int toCol)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool GetDimension(out int fromRow, out int fromCol, out int toRow, out int toCol)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Insert(int rowFrom, int columnFrom, int rows, int columns)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+		#endregion
+
+		#region Private Methods
+
+		private Column GetColumn(int columnPage, int columnPageIndex, bool create)
+		{
+			var page = this.SheetInternal.Pages[columnPage];
+			if (null == page)
+			{
+				if (create)
+					this.SheetInternal.Pages[columnPage] = page = new ColumnsPage();
+				else
+					return null;
+			}
+			var columnObj = page.Columns[columnPageIndex];
+			if (null == columnObj && create)
+			{
+				page.Columns[columnPageIndex] = columnObj = new Column();
+				var column = ZCellStore<T>.RebuildColumn(columnPage, columnPageIndex);
+				if (column > this.MaxUsedColumn)
+					this.MaxUsedColumn = column;
+			}
+			return columnObj;
+		}
+
+		private DataWrapper GetRow(Column column, int rowPage, int rowPageIndex, bool create)
+		{
+			var page = column.Pages[rowPage];
+			if (null == page)
+			{
+				if (create)
+					column.Pages[rowPage] = page = new RowsPage();
+				else
+					return null;
+			}
+			var cell = page.Data[rowPageIndex];
+			if (null == cell && create)
+			{
+				page.Data[rowPageIndex] = cell = new DataWrapper();
+				var row = ZCellStore<T>.RebuildRow(rowPage, rowPageIndex);
+				if (row > this.MaxUsedRow)
+					this.MaxUsedRow = row;
+			}
+			return cell;
+		}
+		#endregion
+
+#if DEBUG
+		public Tuple<int, int, T>[] GetDump()
+		{
+			return null;
+		}
+
+		public void Initialize(Tuple<int, int, T>[] data)
+		{
+			foreach (var datum in data)
+			{
+				var rowValue = datum.Item1;
+				var columnValue = datum.Item2;
+				var dataValue = datum.Item3;
+				ZCellStore<T>.GetColumnCoordinates(columnValue, out int columnPage, out int columnPageIndex);
+				var column = this.GetColumn(columnPage, columnPageIndex, true);
+				ZCellStore<T>.GetRowCoordinates(rowValue, out int rowPage, out int rowPageIndex);
+				var cell = this.GetRow(column, rowPage, rowPageIndex, true);
+				cell.Item = dataValue;
+			}
+		}
+#endif
+
+		#region Internal Static Methods
+		internal static bool GetRowCoordinates(int row, out int rowPage, out int pageIndex)
+		{
+			rowPage = pageIndex = 0;
+			if (row < 1 || row > ExcelPackage.MaxRows)
+				return false;
+			// Rows are 1-index in Excel so let's shift down by one to get into our array-space
+			--row;
+			// Since row pages are in an array of 1024 of we can identify the page by right-shifting 10 (dividing by 1024)
+			rowPage = row >> ZCellStore<T>.RowShift;
+			// Since each page contains 1024 rows, we can take the least significant bits and bitwise AND them to get the index into the actual page.
+			pageIndex = row & ZCellStore<T>.RowMask;
+			return true;
+		}
+
+		internal static bool GetColumnCoordinates(int column, out int columnPage, out int pageIndex)
+		{
+			columnPage = pageIndex = 0;
+			if (column < 1 || column > ExcelPackage.MaxColumns)
+				return false;
+			// Columns are 1-index in Excel so let's shift down by one to get into our array-space
+			--column;
+			// Since column pages are in an array of 128 of we can identify the page by right-shifting 7 (dividing by 128)
+			columnPage = column >> ZCellStore<T>.ColumnShift;
+			// Since each page contains 128 columns, we can take the least significant bits and bitwise AND them to get the index into the actual page.
+			pageIndex = column & ZCellStore<T>.ColumnMask;
+			return true;
+		}
+
+		internal static int RebuildColumn(int columnPage, int pageIndex)
+		{
+			int column = columnPage << ZCellStore<T>.ColumnShift;
+			column = column | ZCellStore<T>.ColumnMask;
+			return column;
+		}
+
+		internal static int RebuildRow(int rowPage, int pageIndex)
+		{
+			int row = rowPage << ZCellStore<T>.RowShift;
+			row = row | ZCellStore<T>.RowMask;
+			return row;
+		}
+		#endregion
+	}
+}
---
+++ EPPlusTest/ZCellStoreTest.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+
+namespace EPPlusTest
+{
+	[TestClass]
+	public class ZCellStoreTest
+	{
+		#region GetRowCoordinates Tests
+		[TestMethod]
+		public void GetRowCoordinates()
+		{
+			// First cell
+			Assert.IsTrue(ZCellStore<object>.GetRowCoordinates(1, out int rowPage, out int pageIndex));
+			Assert.AreEqual(0, rowPage);
+			Assert.AreEqual(0, pageIndex);
+			// Last Cell
+			Assert.IsTrue(ZCellStore<object>.GetRowCoordinates(ExcelPackage.MaxRows, out rowPage, out pageIndex));
+			Assert.AreEqual(1023, rowPage);
+			Assert.AreEqual(1023, pageIndex);
+			// 2nd Page First Cell
+			Assert.IsTrue(ZCellStore<object>.GetRowCoordinates(1024 + 1, out rowPage, out pageIndex));
+			Assert.AreEqual(1, rowPage);
+			Assert.AreEqual(0, pageIndex);
+			// 4th Page 245 Cell
+			Assert.IsTrue(ZCellStore<object>.GetRowCoordinates(1024 + 1024 + 1024 + 245, out rowPage, out pageIndex));
+			Assert.AreEqual(3, rowPage);
+			Assert.AreEqual(244, pageIndex);
+			// Row too small
+			Assert.IsFalse(ZCellStore<object>.GetRowCoordinates(0, out rowPage, out pageIndex));
+			// Row too large
+			Assert.IsFalse(ZCellStore<object>.GetRowCoordinates(ExcelPackage.MaxRows + 1, out rowPage, out pageIndex));
+		}
+		#endregion
+
+		#region GetColumnCoordinates Tests
+		[TestMethod]
+		public void GetColumnCoordinates()
+		{
+			// First column
+			Assert.IsTrue(ZCellStore<object>.GetColumnCoordinates(1, out int columnPage, out int pageIndex));
+			Assert.AreEqual(0, columnPage);
+			Assert.AreEqual(0, pageIndex);
+			// Last column
+			Assert.IsTrue(ZCellStore<object>.GetColumnCoordinates(ExcelPackage.MaxColumns, out columnPage, out pageIndex));
+			Assert.AreEqual(127, columnPage);
+			Assert.AreEqual(127, pageIndex);
+			// 2nd Page First column
+			Assert.IsTrue(ZCellStore<object>.GetColumnCoordinates(128 + 1, out columnPage, out pageIndex));
+			Assert.AreEqual(1, columnPage);
+			Assert.AreEqual(0, pageIndex);
+			// 4th Page 100 column
+			Assert.IsTrue(ZCellStore<object>.GetColumnCoordinates(128 + 128 + 128 + 100, out columnPage, out pageIndex));
+			Assert.AreEqual(3, columnPage);
+			Assert.AreEqual(99, pageIndex);
+			// Column too small
+			Assert.IsFalse(ZCellStore<object>.GetColumnCoordinates(0, out columnPage, out pageIndex));
+			// Column too large
+			Assert.IsFalse(ZCellStore<object>.GetColumnCoordinates(ExcelPackage.MaxColumns + 1, out columnPage, out pageIndex));
+		}
+		#endregion
+
+		#region GetValue Tests
+		[TestMethod]
+		public void GetValue()
+		{
+			var cellStore = new ZCellStore<int>();
+			cellStore.Initialize(new[]
+			{
+				new Tuple<int, int, int>(1, 1, 1),
+				new Tuple<int, int, int>(2, 2, 2),
+				new Tuple<int, int, int>(1024, 10, 3),
+				new Tuple<int, int, int>(1024 + 1024 + 3, 128 + 4, 4),
+				new Tuple<int, int, int>(1024 + 1024 + 1024 + 500, 128 + 128 + 100, 5),
+			});
+			var value = cellStore.GetValue(1, 1);
+			Assert.AreEqual(1, value);
+			value = cellStore.GetValue(2, 2);
+			Assert.AreEqual(2, value);
+			value = cellStore.GetValue(1024, 10);
+			Assert.AreEqual(3, value);
+			value = cellStore.GetValue(1024 + 1024 + 3, 128 + 4);
+			Assert.AreEqual(4, value);
+			value = cellStore.GetValue(1024 + 1024 + 1024 + 500, 128 + 128 + 100);
+			Assert.AreEqual(5, value);
+			// Non-existent value returns default(T)
+			value = cellStore.GetValue(12345, 12345);
+			Assert.AreEqual(0, value);
+		}
+
+		[TestMethod]
+		public void GetValueReturnsDefaultForInvalidCoordinates()
+		{
+			var cellStore = new ZCellStore<int>();
+			// Invalid row too small returns default(T)
+			var value = cellStore.GetValue(0, 10);
+			Assert.AreEqual(0, value);
+			// Invalid row too large returns default(T)
+			value = cellStore.GetValue(ExcelPackage.MaxRows + 1, 10);
+			Assert.AreEqual(0, value);
+			// Invalid column too small returns default(T)
+			value = cellStore.GetValue(10, 0);
+			Assert.AreEqual(0, value);
+			// Invalid column too large returns default(T)
+			value = cellStore.GetValue(10, ExcelPackage.MaxColumns + 1);
+			Assert.AreEqual(0, value);
+		}
+		#endregion
+
+		#region SetValue Tests
+		[TestMethod]
+		public void SetValue()
+		{
+			var cellStore = new ZCellStore<int>();
+			cellStore.Initialize(new[]
+			{
+				new Tuple<int, int, int>(1024 + 1024 + 3, 128 + 4, 4),
+			});
+			var value = cellStore.GetValue(1024 + 1024 + 3, 128 + 4);
+			Assert.AreEqual(4, value);
+			cellStore.SetValue(1024 + 1024 + 3, 128 + 4, 9);
+			value = cellStore.GetValue(1024 + 1024 + 3, 128 + 4);
+			Assert.AreEqual(9, value);
+			// Non-existent value returns default(T)
+			value = cellStore.GetValue(12345, 12345);
+			Assert.AreEqual(0, value);
+			cellStore.SetValue(12345, 12345, 123);
+			value = cellStore.GetValue(12345, 12345);
+			Assert.AreEqual(123, value);
+		}
+
+		[TestMethod]
+		public void SetValueIgnoresInvalidCoordinates()
+		{
+			var cellStore = new ZCellStore<int>();
+			// row too small is ignored
+			cellStore.SetValue(0, 1, 13);
+			// row too large is ignored
+			cellStore.SetValue(ExcelPackage.MaxRows + 1, 1, 13);
+			// column too small is ignored
+			cellStore.SetValue(1, 0, 13);
+			// column too large is ignored
+			cellStore.SetValue(1, ExcelPackage.MaxColumns + 1, 13);
+		}
+		#endregion
+
+		#region Exists Tests
+		[TestMethod]
+		public void Exists()
+		{
+			var cellStore = new ZCellStore<int>();
+			Assert.IsFalse(cellStore.Exists(3, 3));
+			cellStore.SetValue(3, 3, 13);
+			Assert.IsTrue(cellStore.Exists(3, 3));
+			Assert.IsFalse(cellStore.Exists(123984, 12345));
+			cellStore.SetValue(123984, 12345, 16);
+			Assert.IsTrue(cellStore.Exists(123984, 12345));
+		}
+
+		[TestMethod]
+		public void ExistsInvalidCoordinatesReturnFalse()
+		{
+			var cellStore = new ZCellStore<int>();
+			// row too small is false
+			Assert.IsFalse(cellStore.Exists(0, 1));
+			// row too large is false
+			Assert.IsFalse(cellStore.Exists(ExcelPackage.MaxRows + 1, 1));
+			// column too small is false
+			Assert.IsFalse(cellStore.Exists(1, 0));
+			// column too large is false
+			Assert.IsFalse(cellStore.Exists(1, ExcelPackage.MaxColumns + 1));
+		}
+
+		[TestMethod]
+		public void ExistsWithOutValue()
+		{
+			var cellStore = new ZCellStore<int>();
+			Assert.IsFalse(cellStore.Exists(3, 3, out int value));
+			cellStore.SetValue(3, 3, 13);
+			Assert.IsTrue(cellStore.Exists(3, 3, out value));
+			Assert.AreEqual(13, value);
+			Assert.IsFalse(cellStore.Exists(123984, 12345, out value));
+			cellStore.SetValue(123984, 12345, 16);
+			Assert.IsTrue(cellStore.Exists(123984, 12345, out value));
+			Assert.AreEqual(16, value);
+		}
+
+		[TestMethod]
+		public void ExistsWithOutValueInvalidCoordinatesReturnFalse()
+		{
+			var cellStore = new ZCellStore<int>();
+			// row too small is false
+			Assert.IsFalse(cellStore.Exists(0, 1, out int value));
+			// row too large is false
+			Assert.IsFalse(cellStore.Exists(ExcelPackage.MaxRows + 1, 1, out value));
+			// column too small is false
+			Assert.IsFalse(cellStore.Exists(1, 0, out value));
+			// column too large is false
+			Assert.IsFalse(cellStore.Exists(1, ExcelPackage.MaxColumns + 1, out value));
+		}
+		#endregion
+
+		#region NextCell Tests
+		[TestMethod]
+		public void NextCellEmptyStore()
+		{
+			var cellStore = new ZCellStore<int>();
+			int row = 0, column = 0;
+			Assert.IsFalse(cellStore.NextCell(ref row, ref column));
+		}
+
+		[TestMethod]
+		public void NextCellStartsWithZero()
+		{
+			var cellStore = new ZCellStore<int>();
+			cellStore.SetValue(1, 1, 1);
+			int row = 0, column = 0;
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(1, row);
+			Assert.AreEqual(1, column);
+		}
+
+		[TestMethod]
+		public void NextCellFindsNextInRowAcrossPages()
+		{
+			var cellStore = new ZCellStore<int>();
+			cellStore.SetValue(1, 1, 1);
+			cellStore.SetValue(1, 2, 1); // Next on same page
+			cellStore.SetValue(1, 128 + 1, 1); // Next on next page
+			cellStore.SetValue(1, 128 + 128 + 128 + 128 + 50, 1); // Next several pages later
+			int row = 0, column = 0;
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(1, row);
+			Assert.AreEqual(1, column);
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(1, row);
+			Assert.AreEqual(2, column);
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(1, row);
+			Assert.AreEqual(129, column);
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(1, row);
+			Assert.AreEqual(562, column);
+			Assert.IsFalse(cellStore.NextCell(ref row, ref column));
+		}
+
+		[TestMethod]
+		public void NextCellFindsNextInColumnAcrossPages()
+		{
+			var cellStore = new ZCellStore<int>();
+			cellStore.SetValue(1, 1, 1);
+			cellStore.SetValue(2, 1, 1); // Next on same page
+			cellStore.SetValue(1024 + 1, 1, 1); // Next on next page
+			cellStore.SetValue(1024 + 1024 + 1024 + 238, 1, 1); // Next several pages later
+			int row = 0, column = 0;
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(1, row);
+			Assert.AreEqual(1, column);
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(2, row);
+			Assert.AreEqual(1, column);
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(1025, row);
+			Assert.AreEqual(1, column);
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(3310, row);
+			Assert.AreEqual(1, column);
+			Assert.IsFalse(cellStore.NextCell(ref row, ref column));
+		}
+
+		[TestMethod]
+		public void NextCellFindsNextDiagonallyAcrossPages()
+		{
+			var cellStore = new ZCellStore<int>();
+			cellStore.SetValue(1, 1, 1);
+			cellStore.SetValue(2, 2, 1); // Next on same page
+			cellStore.SetValue(1024 + 1, 128 + 1, 1); // Next on next page
+			cellStore.SetValue(1024 + 1024 + 1024 + 238, 128 + 128 + 128 + 1, 1); // Next several pages later
+			int row = 0, column = 0;
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(1, row);
+			Assert.AreEqual(1, column);
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(2, row);
+			Assert.AreEqual(2, column);
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(1025, row);
+			Assert.AreEqual(129, column);
+			Assert.IsTrue(cellStore.NextCell(ref row, ref column));
+			Assert.AreEqual(3310, row);
+			Assert.AreEqual(385, column);
+			Assert.IsFalse(cellStore.NextCell(ref row, ref column));
+		}
+		#endregion
+
+
+		[TestMethod]
+		public void DELME()
+		{
+			var cellstore = new CellStore<int>();
+			cellstore.SetValue(1, 1, 1);
+			cellstore.SetValue(1, 2, 2);
+			cellstore.SetValue(1, 3, 3);
+			cellstore.SetValue(2, 2, 4);
+			cellstore.SetValue(3, 3, 5);
+			int row = 0, column = 0;
+			var found = cellstore.NextCell(ref row, ref column);
+			found = cellstore.NextCell(ref row, ref column);
+			found = cellstore.NextCell(ref row, ref column);
+			found = cellstore.NextCell(ref row, ref column);
+			found = cellstore.NextCell(ref row, ref column);
+			found = cellstore.NextCell(ref row, ref column);
+		}
+	}
+}
