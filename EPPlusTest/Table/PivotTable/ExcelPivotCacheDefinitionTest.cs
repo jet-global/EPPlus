de99fcca6d3826c0ad395e5ea2ba3135acc6eee5
Michelle Lau <michellel@jetreports.com>
2018-09-27 13:35:39 -0700

    Removed UpdateRecordsTest method.

--- EPPlusTest/Table/PivotTable/ExcelPivotCacheDefinitionTest.cs
+++ EPPlusTest/Table/PivotTable/ExcelPivotCacheDefinitionTest.cs
@@ -36,41 +36,6 @@ namespace EPPlusTest.Table.PivotTable
 		}
 		#endregion
 
-		#region UpdateRecords Tests
-		[TestMethod]
-		[DeploymentItem(@"..\..\Workbooks\PivotTableDataSourceTypeWorksheet.xlsx")]
-		public void UpdateRecordsTest()
-		{
-			var file = new FileInfo("PivotTableDataSourceTypeWorksheet.xlsx");
-			Assert.IsTrue(file.Exists);
-			using (var package = new ExcelPackage(file))
-			{
-				var worksheet = package.Workbook.Worksheets.First();
-				var pivotTable = worksheet.PivotTables.First();
-				worksheet.Cells[3, 3].Value = "Item No.";
-				worksheet.Cells[5, 5].Value = "Blue";
-				worksheet.Cells[6, 6].Value = 50;
-				var cacheDefinition = package.Workbook.PivotCacheDefinitions.Single();
-				cacheDefinition.UpdateRecords();
-				Assert.AreEqual(4, cacheDefinition.CacheFields.Count);
-				Assert.AreEqual("Item No.", cacheDefinition.CacheFields.First().Name);
-				var cacheRecords = cacheDefinition.CacheRecords;
-				this.AssertCacheRecord(cacheRecords, 0, 0, PivotCacheRecordType.n, "1");
-				this.AssertCacheRecord(cacheRecords, 1, 0, PivotCacheRecordType.n, "2");
-				this.AssertCacheRecord(cacheRecords, 2, 0, PivotCacheRecordType.n, "3");
-				this.AssertCacheRecord(cacheRecords, 0, 1, PivotCacheRecordType.x, "0");
-				this.AssertCacheRecord(cacheRecords, 1, 1, PivotCacheRecordType.x, "1");
-				this.AssertCacheRecord(cacheRecords, 2, 1, PivotCacheRecordType.x, "2");
-				this.AssertCacheRecord(cacheRecords, 0, 2, PivotCacheRecordType.x, "0");
-				this.AssertCacheRecord(cacheRecords, 1, 2, PivotCacheRecordType.x, "2");
-				this.AssertCacheRecord(cacheRecords, 2, 2, PivotCacheRecordType.x, "0");
-				this.AssertCacheRecord(cacheRecords, 0, 3, PivotCacheRecordType.n, "100");
-				this.AssertCacheRecord(cacheRecords, 1, 3, PivotCacheRecordType.n, "90000");
-				this.AssertCacheRecord(cacheRecords, 2, 3, PivotCacheRecordType.n, "50");
-			}
-		}
-		#endregion
-
 		#region Helper Methods
 		private void AssertCacheRecord(ExcelPivotCacheRecords records, int row, int col, PivotCacheRecordType type, string value)
 		{
