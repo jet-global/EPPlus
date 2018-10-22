08ebe3ce899ff25e444250c90cc20fc6a4df98a4
Michelle Lau <michellel@jetreports.com>
2018-10-22 09:14:37 -0700

    Added test helper classes.

---
+++ EPPlusTest/TestHelpers/ExpectedCellValue.cs
@@ -0,0 +1,24 @@
+﻿namespace EPPlusTest.TestHelpers
+{
+	internal struct ExpectedCellValue
+	{
+		#region Properties
+		public string Sheet { get; set; }
+		public int Row { get; set; }
+		public int Column { get; set; }
+		public object Value { get; set; }
+		public string Formula { get; set; }
+		#endregion
+
+		#region Constructors
+		internal ExpectedCellValue(string sheet, int row, int column, object value, string formula = null)
+		{
+			this.Sheet = sheet;
+			this.Row = row;
+			this.Column = column;
+			this.Value = value;
+			this.Formula = formula;
+		}
+		#endregion
+	}
+}
