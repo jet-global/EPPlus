626cf76eb73f162ad81bac4c7962b76efa30d837
shawncrabtree <shawnc@jetreports.com>
2019-04-15 13:04:47 -0700

    Fixed index out of range, started ignoring hidden items

--- EPPlus/Table/PivotTable/ExcelPivotTable.cs
+++ EPPlus/Table/PivotTable/ExcelPivotTable.cs
@@ -1246,6 +1246,8 @@ namespace OfficeOpenXml.Table.PivotTable
 		private List<Tuple<int, int>> BuildRowItems(PivotItemTreeNode root, List<Tuple<int, int>> indices, List<Tuple<int, int>> lastChildIndices, int indent)
 		{
 			var pivotField = root.PivotFieldItemIndex != -2 ? this.Fields[root.PivotFieldIndex] : null;
+			if (this.IsHidden(root))
+				return indices;
 			int repeatedItemsCount = 0;
 			if (root.Value != -1)
 			{
@@ -1312,8 +1314,16 @@ namespace OfficeOpenXml.Table.PivotTable
 			return null;
 		}
 		
+		private bool IsHidden(PivotItemTreeNode node)
+		{
+			var pivotField = node.PivotFieldItemIndex != -2 ? this.Fields[node.PivotFieldIndex] : null;
+			return pivotField != null && pivotField.Items[node.PivotFieldItemIndex].Hidden;
+		}
+
 		private List<Tuple<int, int>> BuildColumnItems(PivotItemTreeNode node, List<Tuple<int, int>> indices, List<Tuple<int, int>> lastChildIndices)
 		{
+			if (this.IsHidden(node))
+				return indices;
 			int repeatedItemsCount = 0;
 			// Base case (leaf node).
 			if (!node.HasChildren)
@@ -1917,7 +1927,7 @@ namespace OfficeOpenXml.Table.PivotTable
 			else
 			{
 				sharedItemValue = item.Value;
-				if (sharedItemValue != null && sharedItemValue[0] == '<' && DateTime.TryParse(sharedItemValue.Substring(1), out var date))
+				if (!string.IsNullOrEmpty(sharedItemValue) && sharedItemValue[0] == '<' && DateTime.TryParse(sharedItemValue.Substring(1), out var date))
 					sharedItemValue = this.GetItemValueByGroupingType(date, cacheField.FieldGroup.GroupBy);
 			}
 			sharedItemValue = sharedItemValue ?? stringResources.BlankValueHeaderCaption;
