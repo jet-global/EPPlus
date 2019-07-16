4a29966667e8c44a5821543c7363a1d051b7d5cf
mercyvedala <mercy.vedala@insightsoftware.com>
2019-07-16 08:37:53 -0400

    Jet - 262 - Excel SWITCH function: added support in the Jet Hub.

---
+++ EPPlus/FormulaParsing/Excel/Functions/Logical/Switch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OfficeOpenXml.FormulaParsing.ExpressionGraph;
+
+namespace OfficeOpenXml.FormulaParsing.Excel.Functions.Logical
+{
+	/// <summary>
+	/// Represents the Excel SWITCH logical function.
+	/// </summary>
+	public class Switch : ExcelFunction
+	{
+		#region ExcelFunction Overrides
+		/// <summary>
+		/// Executes the SWITCH function.
+		/// </summary>
+		/// <param name="arguments">The arguments to supply to the function.</param>
+		/// <param name="context">The context to evaluate the function in.</param>
+		/// <returns>The <see cref="CompileResult"/> result of executing the function.</returns>
+		public override CompileResult Execute(IEnumerable<FunctionArgument> arguments, ParsingContext context)
+		{
+			if (this.ArgumentsAreValid(arguments, 3, out eErrorType argumentError) == false)
+				return new CompileResult(eErrorType.Value);
+			var value = arguments.First().ValueFirst;
+			var format = ArgToString(arguments, 0);
+			for (int i = 1; i + 1 < arguments.Count(); i += 2)
+			{
+				var valueToMatch = base.ArgToString(arguments, i);
+				if (valueToMatch == format)
+					return new CompileResultFactory().Create(arguments.ElementAt(i + 1).Value);
+			}
+			if (arguments.Count() % 2 == 0)
+			{
+				return new CompileResultFactory().Create(arguments.ElementAt(arguments.Count() - 1).Value);
+			}
+			return new CompileResult(eErrorType.NA);
+		}
+		#endregion
+	}
+}
+
---
+++ EPPlusTest/FormulaParsing/Excel/Functions/Logical/SwitchTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using EPPlusTest.FormulaParsing.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+using OfficeOpenXml.FormulaParsing;
+using OfficeOpenXml.FormulaParsing.Excel.Functions;
+using OfficeOpenXml.FormulaParsing.Excel.Functions.Logical;
+
+namespace EPPlusTest.FormulaParsing.Excel.Functions.Logical
+{
+	[TestClass]
+	public class SwitchTests
+	{
+		#region Class Variables
+		private ParsingContext _parsingContext = ParsingContext.Create();
+		#endregion
+
+		#region TestMethods
+		[TestMethod]
+		public void SwitchFunctionZeroArguments()
+		{
+			var func = new Switch();
+			var result = func.Execute(new List<FunctionArgument>(), _parsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void SwitchFunctionFewArguments()
+		{
+			var function = new Switch();
+			var arguments = FunctionsHelper.CreateArgs(1,1);
+			var result = function.Execute(arguments, _parsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);		
+		}
+
+		[TestMethod]
+		public void SwitchFunctionTooManyArguments()
+		{
+			List<int> list = new List<int>(260);
+			for(int i=0;i<260;i++)
+			{
+				list.Add(i);
+			}
+			var function = new Switch();
+			var arguments = FunctionsHelper.CreateArgs(list);
+			var result = function.Execute(arguments, _parsingContext);
+			Assert.AreEqual(eErrorType.Value, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void SwitchFunctionWithMatchStringArguments()
+		{
+			var function = new Switch();
+			var arguments = FunctionsHelper.CreateArgs("one","one", "Return one", "Default");
+			var result = function.Execute(arguments, _parsingContext);
+			Assert.AreEqual("Return one", result.Result);
+		}
+
+		[TestMethod]
+		public void SwitchFunctionWithNoMatchWithDefault()
+		{
+			var function = new Switch();
+			var arguments = FunctionsHelper.CreateArgs(1, 2, "Two", "Default");
+			var result = function.Execute(arguments, _parsingContext);
+			Assert.AreEqual("Default", result.Result);
+		}
+
+		[TestMethod]
+		public void SwitchFunctionWithNoMatchNoDefault()
+		{
+				var function = new Switch();
+				var arguments = FunctionsHelper.CreateArgs(1, 2, "Two");
+				var result = function.Execute(arguments, _parsingContext);
+				Assert.AreEqual(eErrorType.NA, ((ExcelErrorValue)result.Result).Type);
+		}
+
+		[TestMethod]
+		public void SwitchFunctionWithMatchNoDefault()
+		{
+			var function = new Switch();
+			var arguments = FunctionsHelper.CreateArgs(1, 1, "One");
+			var result = function.Execute(arguments, _parsingContext);
+			Assert.AreEqual("One", result.Result);
+		}
+
+		[TestMethod]
+		public void SwitchFunctionWithMatchDefault()
+		{
+			var function = new Switch();
+			var arguments = FunctionsHelper.CreateArgs(1, 1, "One","Default");
+			var result = function.Execute(arguments, _parsingContext);
+			Assert.AreEqual("One", result.Result);
+		}
+		#endregion
+
+		#region Integration Tests
+		[TestMethod]
+		public void SwitchFunctionIntegrationTrueTest()
+		{
+			using (var package = new ExcelPackage())
+			{
+				var sheet = package.Workbook.Worksheets.Add("Sheet1");
+				sheet.Cells[2, 2].Formula = @"SWITCH(1, 1, ""One"", ""No Match"")";
+				sheet.Calculate();
+				Assert.AreEqual("One", sheet.Cells[2, 2].Value);
+			}
+		}
+		#endregion
+	}
+}
